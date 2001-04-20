bcbf4d345f210847b3ddd5cfa2865b8f21ec7e6c
fuerth <fuerth@49f7741a-0836-0410-9614-f5158065ea1e>
2001-04-20 21:43:45 +0000

    Currently thrown by nothing, but may eventually be used by the User class to indicate a login failure.

---
+++ src/ca/sqlpower/sql/AuthenticationException.java
@@ -0,0 +1,10 @@
+package ca.sqlpower.sql;
+
+/**
+ * gets thrown when an attempt to authenticate a database user fails
+ * because of incorrect credentials.
+ *
+ * @author Jonathan Fuerth
+ * @version $CVS$
+ */
+public class AuthenticationException extends java.lang.Exception {}
