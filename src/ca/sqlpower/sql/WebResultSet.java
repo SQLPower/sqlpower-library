28f6c52150223357e5c4141bd04f9782985a4be9
fuerth <fuerth@49f7741a-0836-0410-9614-f5158065ea1e>
2001-05-01 00:57:44 +0000

    A great deal of work has been done on implementing the Manual Reconciliation screen.  Many classes were affected by this development.

--- src/ca/sqlpower/sql/WebResultSet.java
+++ src/ca/sqlpower/sql/WebResultSet.java
@@ -3,24 +3,54 @@ import java.sql.*;
 
 public class WebResultSet {
     ResultSet rs;
+    ResultSetMetaData rsmd;
+    String sqlQuery;
+    boolean firstColumnIsRowid;
 
-    public WebResultSet(ResultSet results) {
+    public WebResultSet(ResultSet results, String query) throws SQLException {
 	rs=results;
+	rsmd=rs.getMetaData();
+	sqlQuery=query;
+	firstColumnIsRowid=false;
+    }
+
+    public void setFirstColumnIsRowid(boolean flag) {
+	firstColumnIsRowid=flag;
+    }
+
+    public boolean getFirstColumnIsRowid() {
+	return firstColumnIsRowid;
+    }
+
+    public String getSqlQuery() {
+	return sqlQuery;
+    }
+
+    public int getColumnCount() throws SQLException {
+	return rsmd.getColumnCount();
+    }
+
+    public String getColumnLabel(int colNum) throws SQLException {
+	return rsmd.getColumnLabel(colNum);
     }
 
     public boolean next() throws SQLException {
 	return rs.next();
     }
 
-    public String getString(String colName) {
-	throw new UnsupportedOperationException();
+    public String getString(String colName) throws SQLException {
+	return rs.getString(colName);
     }
 
-    public java.sql.Date getDate(String colName) {
-	throw new UnsupportedOperationException();
+    public String getString(int colNum) throws SQLException {
+	return rs.getString(colNum);
     }
 
-    public float getFloat(String colName) {
-	throw new UnsupportedOperationException();
+    public java.sql.Date getDate(String colName) throws SQLException {
+	return rs.getDate(colName);
+    }
+
+    public float getFloat(String colName) throws SQLException {
+	return rs.getFloat(colName);
     }
 }
