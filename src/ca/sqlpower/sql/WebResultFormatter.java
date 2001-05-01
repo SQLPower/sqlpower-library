5bec446ec7beb483201d6cf72f89a4e269e9b86f
fuerth <fuerth@49f7741a-0836-0410-9614-f5158065ea1e>
2001-05-01 20:02:54 +0000

    Added an abstract formatToStream class to the WebResultFormatter class, and made the necessary changes where it was used.

--- src/ca/sqlpower/sql/WebResultFormatter.java
+++ src/ca/sqlpower/sql/WebResultFormatter.java
@@ -1,4 +1,9 @@
 package ca.sqlpower.sql;
 
+import java.io.*;
+import java.sql.SQLException;
+
 public abstract class WebResultFormatter {
+    public abstract void formatToStream(WebResultSet wrs, PrintWriter out) 
+	throws SQLException;
 }
