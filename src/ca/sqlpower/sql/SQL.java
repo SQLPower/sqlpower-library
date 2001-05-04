de84bd5561ab6019a2694b6cdb5e088154eeb94c
fuerth <fuerth@49f7741a-0836-0410-9614-f5158065ea1e>
2001-05-04 19:45:46 +0000

    Initial revision.

---
+++ src/ca/sqlpower/sql/SQL.java
@@ -0,0 +1,32 @@
+package ca.sqlpower.sql;
+
+public class SQL {
+
+    /**
+     * This class cannot be instantiated
+     */
+    private SQL()
+    {
+    }
+
+    public static String escapeStatement(String old) 
+    { 
+	// a premature optimisation 
+	if(old.lastIndexOf('\'') == -1) { 
+	    return old; 
+	} 
+	
+	int i=0; 
+	StringBuffer escaped=new StringBuffer(old); 
+	
+	while(i < escaped.length()) 
+	    { 
+		if(escaped.charAt(i)=='\'') { 
+		    escaped.insert(i, '\''); 
+		    i++;  // skip over the added quote 
+		} 
+		i++; 
+	    } 
+	return(escaped.toString()); 
+    } 
+}
---
+++ src/ca/sqlpower/sql/WebResultFormFormatter.java
@@ -0,0 +1,72 @@
+package ca.sqlpower.sql;
+
+import ca.sqlpower.util.*;
+import java.util.*;
+import java.sql.*;
+import java.io.*;
+
+/**
+ * WebResultFormFormatter exists in order to format WebResultSets into
+ * a set of HTML form elements.  You may call formatToStream on the
+ * same instance as many times as you like; the output settings will
+ * remain in effect until you change them.
+ *
+ * @author Jonathan Fuerth
+ * @version $Id$
+ */
+public class WebResultFormFormatter extends WebResultFormatter {
+
+    private int numHTMLCols;
+    
+    public WebResultFormFormatter() {
+	super();
+	numHTMLCols=3;
+    }
+
+    public void setNumHTMLCols(int numCols) {
+	numHTMLCols=numCols;
+    }
+
+    public int getNumHTMLCols() {
+	return numHTMLCols;
+    }
+
+    public void formatToStream(WebResultSet wrs, PrintWriter out) 
+	throws SQLException {
+	int numCols=wrs.getColumnCount();
+	int cell=0, col=0;;
+	boolean fcRowid=wrs.getFirstColumnIsRowid();
+
+	if(!wrs.next()) {
+	    return;
+	}
+
+	out.println("<table>");
+	out.println(" <tr>");
+	do {
+	    if(fcRowid && cell==0) {
+		cell++;
+	    }
+	    out.println("  <td align=\"right\">");
+	    out.println(beautifyHeading(wrs.getColumnLabel(cell+1)));
+	    out.println("  </td>");
+	    out.println("  <td>");
+	    out.print("   <input type=\"text\" name=\"");
+	    out.print(wrs.getColumnLabel(cell+1));
+	    out.print("\" value=\"");
+	    out.print(wrs.getString(cell+1));
+	    out.println("\" />");
+	    out.println("  </td>");
+	    if(col==numHTMLCols-1) {
+		col=0;
+		out.println(" </tr>");
+		out.println(" <tr>");
+	    }
+	    col++;
+	    cell++;
+	} while(cell<numCols);
+	out.println(" </tr>");
+	out.println("</table>");
+	out.flush();
+    }
+}
