3303fdbf170e35507729ac8182c4abeebe315ab6
fuerth <fuerth@49f7741a-0836-0410-9614-f5158065ea1e>
2001-05-10 23:18:24 +0000

    New exception type meant for use within this package.

---
+++ src/ca/sqlpower/sql/ColumnNotDisplayableException.java
@@ -0,0 +1,10 @@
+package ca.sqlpower.sql;
+
+/**
+ * Throwing a ColumnNotDisplayableException indicates that a column
+ * for which data was requested is not meant for screen display.
+ *
+ * @author Jonathan Fuerth
+ * @version $Id$
+ */
+public class ColumnNotDisplayableException extends Exception {}
