9c78f0cc18a1bdc5e573811ce7cc2c91c2ba9b34
fuerth <fuerth@49f7741a-0836-0410-9614-f5158065ea1e>
2001-06-14 21:33:34 +0000

    -Added javadocs -Added new method to escape and quote a string

--- src/ca/sqlpower/sql/SQL.java
+++ src/ca/sqlpower/sql/SQL.java
@@ -9,8 +9,33 @@ public class SQL {
     {
     }
 
+    /**
+     * A convent way of using escapeStatement.  This method does the
+     * same thing as escapeStatement, but also encloses the returned
+     * string in single-quotes.
+     *
+     * @param string The string that you want escaped and quoted.
+     * @return The same as escapeStatement would, but enclosed in quotes.
+     */
+    public static String quote(String string) {
+	return "'"+escapeStatement(string)+"+";
+    }
+
+    /**
+     * Makes the input string safe to enclose in single-quotes in an
+     * Oracle SQL expression.  Currently, this only means turning all
+     * "'" characters into the "''" escape sequence.
+     *
+     * @param old The original string
+     * @return The awk/perl substitution on <code>old</code> of
+     * <code>s/'/''/g</code>
+     */
     public static String escapeStatement(String old) 
     { 
+	if(old==null) {
+	    return "null";
+	}
+
 	// a premature optimisation 
 	if(old.lastIndexOf('\'') == -1) { 
 	    return old; 
@@ -30,6 +55,13 @@ public class SQL {
 	return(escaped.toString()); 
     } 
 
+    /**
+     * Converts the character representation of a YES/NO value into
+     * boolean.
+     *
+     * @param indicator A string that says YES or NO or Y or N.
+     * @return true iff <code>indicator.charAt(0)=='Y'</code>.
+     */
     public static boolean decodeInd(String indicator) {
 	if(indicator.charAt(0) == 'Y') {
 	    return true;
