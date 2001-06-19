b67489e6a680820d0c24f2e842062ee85f723f7b
fuerth <fuerth@49f7741a-0836-0410-9614-f5158065ea1e>
2001-06-19 20:05:31 +0000

    -Changed filter behaviour: it is now possible to force a leading and trailing wildcard character, and this behaviour is on by default

--- src/ca/sqlpower/sql/NonNumericToWildcardFilter.java
+++ src/ca/sqlpower/sql/NonNumericToWildcardFilter.java
@@ -4,9 +4,23 @@ public class NonNumericToWildcardFilter implements ColumnFilter {
     static final int STATE_NUMBERS=0;
     static final int STATE_NON_NUMBERS=1;
     char wildcard='%';
+    boolean leadingWildcard=false;
+    boolean trailingWildcard=false;
 
     public static ColumnFilter getInstance() {
 	return (ColumnFilter)new NonNumericToWildcardFilter();
+    }
+
+    public void setWildcardCharacter(char wild) {
+	wildcard=wild;
+    }
+
+    public void setForceLeadingWildcard(boolean enabled) {
+	leadingWildcard=enabled;
+    }
+
+    public void setForceTrailingWildcard(boolean enabled) {
+	trailingWildcard=enabled;
     }
 
     public String filter(String in) {
@@ -26,6 +40,15 @@ public class NonNumericToWildcardFilter implements ColumnFilter {
 		state=STATE_NUMBERS;
 	    }
 	}
+
+	if(out.charAt(0) != wildcard) {
+	    out.insert(0, wildcard);
+	}
+
+	if(out.charAt(out.length()-1) != wildcard) {
+	    out.append(wildcard);
+	}
+
 	return out.toString();
     }
 
