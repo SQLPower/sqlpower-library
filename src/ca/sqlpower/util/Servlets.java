bc7217d1e09cce1c157e42108bb31f25739fc2cf
fuerth <fuerth@49f7741a-0836-0410-9614-f5158065ea1e>
2002-04-29 21:42:45 +0000

    -Currently includes a method for printing out a list of request  parameters.

---
+++ src/ca/sqlpower/util/Servlets.java
@@ -0,0 +1,62 @@
+package ca.sqlpower.util;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import java.util.*;
+import java.io.*;
+
+/**
+ * Just a collection of methods for dealing with common servlet tasks.
+ *
+ * @author Jonathan Fuerth
+ * @version $Id$
+ */
+public class Servlets {
+
+
+	/**
+	 * Prints all the parameter names and values, one parameter per
+	 * line, to the given print writer.
+	 *
+	 * @param out The PrintWriter to send the formatted request data to.
+	 * @param req The servlet request object you want to examine.
+	 */
+	public static void printRequest(PrintStream out, ServletRequest req) {
+		Enumeration params=req.getParameterNames();
+		while(params.hasMoreElements()) {
+			String param=(String)params.nextElement();
+			out.print(param);
+			out.print(": ");
+			out.println(arrayToString(req.getParameterValues(param)));
+		}
+	}
+
+	/**
+	 * Returns a comma-separated list of the <code>toString()</code>
+	 * value of each entry of the array.
+	 *
+	 * @param array An array of objects.  The <code>toString()</code>
+	 * method will be called on each non-<code>null</code> entry.
+	 * @return A string representation of the array, or <code>"(null
+	 * array)"</code> if the <code>array</code> argument is
+	 * <code>null</code>.
+	 */
+	public static String arrayToString(Object[] array) {
+		if(array==null) {
+			return "(null array)";
+		}
+		StringBuffer arrayString=new StringBuffer();
+		boolean firstItem=true;
+		for(int i=0; i<array.length; i++) {
+			if(!firstItem) {
+				arrayString.append(", ");
+			}
+			if(array[i]==null) {
+				arrayString.append("(null entry)");
+			} else {
+				arrayString.append(array[i].toString());
+			}
+		}
+		return arrayString.toString();
+	}
+}
