87009e8e5cbf0f1de0a70e139b3ca7d19918d08a
fuerth <fuerth@49f7741a-0836-0410-9614-f5158065ea1e>
2002-10-18 18:02:51 +0000

    -First revision.. uses hash table of column names->values

---
+++ src/ca/sqlpower/sql/DefaultParameters.java
@@ -0,0 +1,57 @@
+package ca.sqlpower.sql;
+
+import java.sql.*;
+import java.util.*;
+
+/**
+ * Interface to the all-knowing, all-powerful DEF_PARAM table.
+ *
+ * <p><code>END OF LINE.</code>
+ */
+public class DefaultParameters {
+
+	Map params;
+
+	public DefaultParameters(Connection con) throws SQLException, PLSchemaException {
+		StringBuffer sql = new StringBuffer();
+		sql.append("SELECT * FROM DEF_PARAM");
+
+		Statement stmt = null;
+		ResultSet rs = null;
+		try {
+			stmt = con.createStatement();
+			rs = stmt.executeQuery(sql.toString());
+			if(!rs.next()) {
+				throw new PLSchemaException("There are no rows in DEF_PARAM!!!!!!!!!?!?!!!!");
+			}
+			params = new HashMap();
+			ResultSetMetaData rsmd = rs.getMetaData();
+			for(int col=1, ncols=rsmd.getColumnCount(); col<=ncols; col++) {
+				params.put(rsmd.getColumnName(col).toLowerCase(), rs.getString(col));
+			}
+		} finally {
+			if(rs != null) {
+				rs.close();
+			}
+			if(stmt != null) {
+				stmt.close();
+			}
+		}
+	}
+
+	/**
+	 * Gets the specified parameter from (our snapshot of) the
+	 * DEF_PARAM table.
+	 *
+	 * @param paramName The column name from DEF_PARAM.
+	 * @throws IllegalArgumentException if the given parameter isn't a
+	 * column name in DEF_PARAM
+	 */
+	public String get(String paramName) {
+		String lcParamName = paramName.toLowerCase();
+		if(!params.containsKey(lcParamName)) {
+			throw new IllegalArgumentException("DEF_PARAM version "+params.get("SCHEMA_VERSION")+" doesn't have the parameter "+paramName);
+		}
+		return (String) params.get(paramName.toLowerCase());
+	}
+}
