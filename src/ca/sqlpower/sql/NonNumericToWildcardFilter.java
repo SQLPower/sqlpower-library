958eae66ff69758735ea008251fe1201bd44d88a
fuerth <fuerth@49f7741a-0836-0410-9614-f5158065ea1e>
2003-01-07 17:08:29 +0000

    -Fixed indenting for tab width of 4

--- src/ca/sqlpower/sql/NonNumericToWildcardFilter.java
+++ src/ca/sqlpower/sql/NonNumericToWildcardFilter.java
@@ -8,48 +8,47 @@ public class NonNumericToWildcardFilter implements ColumnFilter {
     boolean trailingWildcard=false;
 
     public static ColumnFilter getInstance() {
-	return (ColumnFilter)new NonNumericToWildcardFilter();
+		return (ColumnFilter)new NonNumericToWildcardFilter();
     }
 
     public void setWildcardCharacter(char wild) {
-	wildcard=wild;
+		wildcard=wild;
     }
 
     public void setForceLeadingWildcard(boolean enabled) {
-	leadingWildcard=enabled;
+		leadingWildcard=enabled;
     }
 
     public void setForceTrailingWildcard(boolean enabled) {
-	trailingWildcard=enabled;
+		trailingWildcard=enabled;
     }
 
     public String filter(String in) {
-	char ch;
-	int state=STATE_NUMBERS;
-	StringBuffer out=new StringBuffer(in.length());
-
-	for(int i=0; i<in.length(); i++) {
-	    ch=in.charAt(i);
-	    if(ch < '0' || ch > '9') {
-		if(state==STATE_NUMBERS) {
-		    out.append(wildcard);
+		char ch;
+		int state=STATE_NUMBERS;
+		StringBuffer out=new StringBuffer(in.length());
+		
+		for(int i=0; i<in.length(); i++) {
+			ch=in.charAt(i);
+			if(ch < '0' || ch > '9') {
+				if(state==STATE_NUMBERS) {
+					out.append(wildcard);
+				}
+				state=STATE_NON_NUMBERS;
+			} else {
+				out.append(ch);
+				state=STATE_NUMBERS;
+			}
 		}
-		state=STATE_NON_NUMBERS;
-	    } else {
-		out.append(ch);
-		state=STATE_NUMBERS;
-	    }
-	}
-
-	if(out.charAt(0) != wildcard) {
-	    out.insert(0, wildcard);
-	}
-
-	if(out.charAt(out.length()-1) != wildcard) {
-	    out.append(wildcard);
-	}
-
-	return out.toString();
+		
+		if(out.charAt(0) != wildcard) {
+			out.insert(0, wildcard);
+		}
+		
+		if(out.charAt(out.length()-1) != wildcard) {
+			out.append(wildcard);
+		}
+		
+		return out.toString();
     }
-
 }
