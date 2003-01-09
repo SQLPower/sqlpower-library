69fd0ee00dc5dc17801928787ab9416f65be242a
fuerth <fuerth@49f7741a-0836-0410-9614-f5158065ea1e>
2003-01-09 21:58:11 +0000

    -Factored out the generic version stuff to the ca.sqlpower.util.Version class, which SchemaVersion now extends. -Same for SchemaVersionFormatException.

--- src/ca/sqlpower/sql/SchemaVersion.java
+++ src/ca/sqlpower/sql/SchemaVersion.java
@@ -1,21 +1,15 @@
 package ca.sqlpower.sql;
 
-import java.io.Serializable;
 import java.sql.*;
-import java.util.*;
+import ca.sqlpower.util.VersionFormatException;
 
-public class SchemaVersion implements Comparable, Serializable {
-	int major;
-	int minor;
-	int tiny;
+public class SchemaVersion extends ca.sqlpower.util.Version {
 
 	/**
 	 * Creates a new SchemaVersion object with all three components set to -1.
 	 */
 	public SchemaVersion() {
-		major = -1;
-		minor = -1;
-		tiny = -1;
+		super();
 	}
 
 	/**
@@ -23,9 +17,7 @@ public class SchemaVersion implements Comparable, Serializable {
 	 * versions as specified.
 	 */
 	public SchemaVersion(int major, int minor, int tiny) {
-		this.major = major;
-		this.minor = minor;
-		this.tiny = tiny;
+		super(major, minor, tiny);
 	}
 
 	public static SchemaVersion makeFromDatabase(Connection con)
@@ -40,124 +32,13 @@ public class SchemaVersion implements Comparable, Serializable {
 				version = new SchemaVersion();
 				version.setVersion(versionString);
 			}
+		} catch (VersionFormatException e) {
+			throw new SchemaVersionFormatException(e.getMessage(), 0, e.getCause());
 		} finally {
 			if(stmt != null) {
 				stmt.close();
 			}
 		}
 		return version;
-	}
-	
-	/**
-	 * Gets the value of major
-	 *
-	 * @return the value of major
-	 */
-	public int getMajor() {
-		return this.major;
-	}
-
-	/**
-	 * Sets the value of major
-	 *
-	 * @param argMajor Value to assign to this.major
-	 */
-	public void setMajor(int argMajor){
-		this.major = argMajor;
-	}
-
-	/**
-	 * Gets the value of minor
-	 *
-	 * @return the value of minor
-	 */
-	public int getMinor() {
-		return this.minor;
-	}
-
-	/**
-	 * Sets the value of minor
-	 *
-	 * @param argMinor Value to assign to this.minor
-	 */
-	public void setMinor(int argMinor){
-		this.minor = argMinor;
-	}
-
-	/**
-	 * Gets the value of tiny
-	 *
-	 * @return the value of tiny
-	 */
-	public int getTiny() {
-		return this.tiny;
-	}
-
-	/**
-	 * Sets the value of tiny
-	 *
-	 * @param argTiny Value to assign to this.tiny
-	 */
-	public void setTiny(int argTiny){
-		this.tiny = argTiny;
-	}
-
-	public void setVersion(String versionString) throws SchemaVersionFormatException {
-		StringTokenizer st = new StringTokenizer(versionString, ".");
-		try {
-			major = Integer.parseInt(st.nextToken());
-			minor = Integer.parseInt(st.nextToken());
-			tiny = Integer.parseInt(st.nextToken());
-		} catch(NumberFormatException e) {
-			throw new SchemaVersionFormatException("A component of the version string '"
-												   +versionString+"' is not an integer",
-												   0, e);
-		} catch(NoSuchElementException e) {
-			throw new SchemaVersionFormatException("The version string '"+versionString
-												   +"' has fewer than three components",
-												   0, e);
-		}
-
-		if(st.hasMoreTokens()) {
-			throw new SchemaVersionFormatException("The version string '"+versionString
-												   +"' has too many components",
-												   0, null);
-		}
-	}
-
-	public String getVersion() {
-		return new StringBuffer()
-			.append(major)
-			.append(".")
-			.append(minor)
-			.append(".")
-			.append(tiny).toString();
-	}
-
-	public String toString() {
-		return getVersion();
-	}
-
-	public int compareTo(Object other) {
-		SchemaVersion otherVersion = (SchemaVersion) other;
-		if(this.major < otherVersion.major) {
-			return -1;
-		}
-		if(this.major > otherVersion.major) {
-			return 1;
-		}
-		if(this.minor < otherVersion.minor) {
-			return -1;
-		}
-		if(this.minor > otherVersion.minor) {
-			return 1;
-		}
-		if(this.tiny < otherVersion.tiny) {
-			return -1;
-		}
-		if(this.tiny > otherVersion.tiny) {
-			return 1;
-		}
-		return 0;
 	}
 }
