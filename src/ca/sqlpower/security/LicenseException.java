6b7db424a46fbac9e114e49402f6235d70bf8372
fuerth <fuerth@49f7741a-0836-0410-9614-f5158065ea1e>
2003-08-18 17:37:21 +0000

    Fixed bug 395 by making license exceptions internationalizable (including new messages in ApplicationResources.properties)

--- src/ca/sqlpower/security/LicenseException.java
+++ src/ca/sqlpower/security/LicenseException.java
@@ -1,30 +1,102 @@
 package ca.sqlpower.security;
 
+import java.util.List;
+import java.util.ArrayList;
+
 /**
- * This exception is meant to be thrown whenever an application
- * detects a violation of a valid license.  It is not intended to be
- * thrown when an invalid (corrupt, tampered-with) license is
- * detected.  See {@link LicenseReadException}.
+ * This exception (or a subclass) is meant to be thrown whenever an
+ * application detects a violation of a valid license.  It is not
+ * intended to be thrown when an invalid (corrupt, tampered-with)
+ * license is detected.  See {@link LicenseReadException}.
  *
  * @author Jonathan Fuerth
  * @version $Id$
  */
 public class LicenseException extends Exception {
-	Throwable cause;
-	String message;
+	protected String reasonCode;
+	protected List errorParams;
+	protected Throwable cause;
 
-	public LicenseException(String message, Throwable cause) {
+	/**
+	 * The most generic constructor.  Takes an ApplicationResources
+	 * key (the reason code), a List of substitution objects (which
+	 * should be used for the substitution parameters that format this
+	 * exception for the user), and an optional nested cause for this
+	 * license exception.
+	 */
+	public LicenseException(String reasonCode, List errorParams, Throwable cause) {
 		super();
+		this.reasonCode = reasonCode;
+		this.errorParams = errorParams;
 		this.cause = cause;
-		this.message = message;
 	}
 
-	public LicenseException(String message) {
-		super();
-		this.cause = null;
-		this.message = message;
+	public LicenseException(String reasonCode, Throwable cause) {
+		this(reasonCode, null, cause);
 	}
 
-	public Throwable getCause() { return cause; }
-	public String getMessage()  { return message; }
+	public LicenseException(String reasonCode, Object errorParam) {
+		this(reasonCode, null, null);
+		if (errorParam != null) {
+			errorParams = new ArrayList(1);
+			errorParams.add(errorParam);
+		}
+	}
+
+	public LicenseException(String reasonCode) {
+		this(reasonCode, null, null);
+	}
+
+	public Throwable getCause() {
+		return cause;
+	}
+
+	/**
+	 * Returns the reason code, which should be translated to
+	 * human-readable form via a language-specific
+	 * ApplicationResources.properties file.
+	 */
+	public String getReasonCode() {
+		return reasonCode;
+	}
+
+	/**
+	 * Returns the ith error parameter, or null if there is no such
+	 * parameter.
+	 */
+	public Object getErrorParam(int i) { 
+		if (errorParams != null && i < errorParams.size()) {
+			return errorParams.get(i);
+		} else {
+			return null;
+		}
+	}
+
+	/**
+	 * Tells how many error parameters there are in this exception.
+	 */
+	public int getErrorParamCount() {
+		if (errorParams != null) {
+			return errorParams.size();
+		} else {
+			return 0;
+		}
+	}
+
+	/**
+	 * Useful if you're using the Jakarta Struts ActionMessage class
+	 * for formatting.
+	 */
+	public Object[] getErrorParamArray() {
+		int n = getErrorParamCount();
+		if (n == 0) {
+			return null;
+		} else {
+			Object[] params = new Object[n];
+			for (int i = 0; i < n; i++) {
+				params[i] = errorParams.get(i);
+			}
+			return params;
+		}
+	}
 }
