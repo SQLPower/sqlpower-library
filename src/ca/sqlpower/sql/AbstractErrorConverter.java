b06baaf0f51835bc981a1c0dce8870ee87ce8e49
gillian <gillian@49f7741a-0836-0410-9614-f5158065ea1e>
2004-03-04 14:30:14 +0000

    I don't know what this is - a change Jonathan made at Warren Shepell

--- src/ca/sqlpower/sql/AbstractErrorConverter.java
+++ src/ca/sqlpower/sql/AbstractErrorConverter.java
@@ -1,51 +1,156 @@
 package ca.sqlpower.sql;
 
+
+
+
+
 import java.sql.*;
 
+
+
+
+
 /**
+
+
  * Handles the conversion of database-specific error numbers within 
+
+
  * SQLExceptions to custom SQLPower error numbers.
+
+
  */
+
+
 public abstract class AbstractErrorConverter {
 
+
+
+
+
 	/**
+
+
 	 * Used if database connection fails because of invalid
+
+
 	 * username or password.
+
+
 	 */
+
+
 	public static final int INVALID_LOGON = 1;
 
+
+
+
+
 	/**
+
+
 	 * Used if a query fails due to a missing or unknown column name.
+
+
 	 */
+
+
 	public static final int UNKNOWN_COLUMN = 2;
 
+
+
+
+
 	/**
+
+
 	 * Used if the driver can't connect to the database server at all.
+
+
 	 */
+
+
 	public static final int SERVER_UNAVAILABLE = 3;
 
+
+
+
+
 	/**
+
+
 	 * Used if the PLErrorConverter is asked to convert an exception.
+
+
 	 * Means the schema is too old or just plain missing.
+
+
 	 */
+
+
 	public static final int BAD_PL_SCHEMA = 4;
 
+
+
+
+
 	/**
+
+
 	 * Used if the DMBS or JDBC driver throws an exception due to a
+
+
 	 * syntax error in a SQL query.
+
+
 	 */
+
+
 	public static final int SQL_SYNTAX_ERROR = 5;
 
-	/** 
-	 * Used if an appropriate conversion for the database error
-	 * cannot be found.
-	 */
-	public static final int UNKNOWN_ERROR = -1;
-	
+
 	/**
-	 * Converts a SQLException to a SQLPower error code.
+	 * Used for RDBMS unknown table/view errors.
 	 */
+	public static final int UNKNOWN_TABLE = 6;
+
+
+	/** 
+
+
+	 * Used if an appropriate conversion for the database error
+
+
+	 * cannot be found.
+
+
+	 */
+
+
+	public static final int UNKNOWN_ERROR = -1;
+
+
+	
+
+
+	/**
+
+
+	 * Converts a SQLException to a SQLPower error code.
+
+
+	 */
+
+
 	public abstract int convert(SQLException e);
 
+
+
+
+
 }
+
+
+
+
 
--- src/ca/sqlpower/sql/SQLServerErrorConverter.java
+++ src/ca/sqlpower/sql/SQLServerErrorConverter.java
@@ -1,31 +1,94 @@
 package ca.sqlpower.sql;
 
+
+
+
+
 import java.sql.SQLException;
 
+
+
+
+
 /**
+
+
  * Converts SQLExceptions from SQL Server into SQLPower error numbers.
+
+
  */
+
+
 public class SQLServerErrorConverter extends AbstractErrorConverter {
+
+
 	
+
+
 	public int convert(SQLException e) {
+
+
 		switch (e.getErrorCode()) {
+
+
 		case 0:
+
+
 			// this might be a problem: we get error code "0" for
+
+
 			// "error establishing the socket," which is likely not
+
+
 			// unique.
+
+
 			return SERVER_UNAVAILABLE;
 
+
+
+
+
 		case 207:
+
+
 			return UNKNOWN_COLUMN;
 
+
+		case 208:
+			return UNKNOWN_TABLE;
+
+
 		case 18456:
+
+
 		case 4060:
+
+
 		case 18452:
+
+
 			return INVALID_LOGON;
 
+
+
+
+
 		default:
+
+
 			return UNKNOWN_ERROR;
+
+
 		}
+
+
 	}
+
+
 }
+
+
+
+
 
