d78bac842016d4ff5199efe44b6dd0d7b3ea7019
fuerth <fuerth@49f7741a-0836-0410-9614-f5158065ea1e>
2005-11-08 20:30:45 +0000

    Added cache support to reduce the amount of XML parsing that needs to be done.

--- src/ca/sqlpower/sql/XMLFileDBCSSource.java
+++ src/ca/sqlpower/sql/XMLFileDBCSSource.java
@@ -7,16 +7,34 @@ import java.util.*;
  * A plain XML-file-based source for the list of DBConnectionSpec
  * objects that should be presented to the user when they need to pick
  * a database to connect to.
+ * <p>
+ * Each instance of this class keeps a cached copy of the connection info
+ * list, and will automatically reload the list from the file if its 
+ * modification time changes. 
  *
  * @author Jonathan Fuerth
  * @version $Id$
  */
 public class XMLFileDBCSSource implements DBCSSource, Serializable {
 
-	protected String xmlFileName=null;
+	private String xmlFileName;
+    
+    /**
+     * A cached copy of the last DBCS List we returned.  Profiling shows that
+     * parsing the XML file every time we get to the login screen is quite
+     * wasteful of memory.
+     */
+    private List cachedDbcsList;
 
+    /**
+     * The time (from System.currentTimeMillis()) that the cachedDbcsList was
+     * last read from the file.  We compare this with the file's modification
+     * time to see if it needs to be reloaded.
+     */
+    private long cachedDbcsListRefreshTime;
+    
 	public XMLFileDBCSSource(String xmlFileName) {
-		this.xmlFileName=xmlFileName;
+		this.xmlFileName = xmlFileName;
 	}
 
 	/**
@@ -26,24 +44,25 @@ public class XMLFileDBCSSource implements DBCSSource, Serializable {
 	 * @throws DatabaseListReadException if there is a problem reading
 	 * the database list from the XML file.
 	 */
-	public List getDBCSList()
-		throws IllegalStateException, DatabaseListReadException {
-
-		List dbcsList=null;
-		InputStream dbXMLFile=null;
-
-		try {
-			dbXMLFile = new BufferedInputStream(new FileInputStream(xmlFileName));
-			dbcsList=new ArrayList(DBCSSourceSupport.getListUsingXMLStream(dbXMLFile));
-		} catch(IOException e) {
-			throw new DatabaseListReadException(e);
-		} finally {
-			try {
-				dbXMLFile.close();
-			} catch(IOException e) {
-				throw new DatabaseListReadException(e);
-			}
-		}
-		return dbcsList;
+	public synchronized List getDBCSList() throws IllegalStateException, DatabaseListReadException {
+	    File dbcsFile = new File(xmlFileName);
+	    if (cachedDbcsList == null || dbcsFile.lastModified() >= cachedDbcsListRefreshTime) {
+	        InputStream dbXMLFile = null;
+	        
+	        try {
+	            dbXMLFile = new BufferedInputStream(new FileInputStream(dbcsFile));
+	            cachedDbcsList = new ArrayList(DBCSSourceSupport.getListUsingXMLStream(dbXMLFile));
+	            cachedDbcsListRefreshTime = System.currentTimeMillis();
+	        } catch (IOException e) {
+	            throw new DatabaseListReadException(e);
+	        } finally {
+	            try {
+	                dbXMLFile.close();
+	            } catch(IOException e) {
+	                throw new DatabaseListReadException(e);
+	            }
+	        }
+	    }
+	    return cachedDbcsList;
 	}
 }
