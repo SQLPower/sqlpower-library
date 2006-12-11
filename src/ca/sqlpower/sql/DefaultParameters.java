906153f876e2165529a1fcb2b2b312f6538fc0b9
fuerth <fuerth@49f7741a-0836-0410-9614-f5158065ea1e>
2006-12-11 21:08:03 +0000

    Added new constructor that supports optional schema and catalog parameters.

--- src/ca/sqlpower/sql/DefaultParameters.java
+++ src/ca/sqlpower/sql/DefaultParameters.java
@@ -3,6 +3,9 @@ package ca.sqlpower.sql;
 import java.sql.*;
 import java.util.*;
 
+import ca.sqlpower.util.Version;
+import ca.sqlpower.util.VersionFormatException;
+
 /**
  * Interface to the all-knowing, all-powerful DEF_PARAM table.
  *
@@ -12,28 +15,59 @@ public class DefaultParameters {
 
 	Map params;
 
-	public DefaultParameters(Connection con) throws SQLException, PLSchemaException {
+    /**
+     * Creates a DefaultParameters instance based on the values stored in
+     * the DEF_PARAM table in the given connection's current catalog and schema.
+     * 
+     * @param con The connection to read parameters from.
+     * @throws SQLException If the DEF_PARAM table is missing or otherwise not accessible
+     * @throws PLSchemaException If the DEF_PARAM.SCHEMA_VERSION column does not contain
+     *          a major.minor.tiny style version number.
+     */
+    public DefaultParameters(Connection con) throws SQLException, PLSchemaException {
+        this(con, null, null);
+    }
+
+    /**
+     * Creates a DefaultParameters instance based on the values stored in
+     * the DEF_PARAM table in the given catalog and schema.
+     * 
+     * @param con The connection to read parameters from.
+     * @param plCatalog The catalog to read from (null means omit the catalog from the query)
+     * @param plSchema The schema to read from (null means omit the schema from the query)
+     * @throws SQLException If the DEF_PARAM table is missing or otherwise not accessible
+     * @throws PLSchemaException If the DEF_PARAM.SCHEMA_VERSION column does not contain
+     *          a major.minor.tiny style version number.
+     */
+    public DefaultParameters(Connection con, String plCatalog, String plSchema) throws SQLException, PLSchemaException {
 		StringBuffer sql = new StringBuffer();
-		sql.append("SELECT * FROM DEF_PARAM");
+		sql.append("SELECT * FROM ");
+        if (plCatalog != null) {
+            sql.append(plCatalog).append(".");
+        }
+        if (plSchema != null) {
+            sql.append(plSchema).append(".");
+        }
+        sql.append("DEF_PARAM");
 
 		Statement stmt = null;
 		ResultSet rs = null;
 		try {
 			stmt = con.createStatement();
 			rs = stmt.executeQuery(sql.toString());
-			if(!rs.next()) {
-				throw new PLSchemaException("There are no rows in DEF_PARAM!!!!!!!!!?!?!!!!");
+			if (!rs.next()) {
+				throw new PLSchemaException("There are no rows in DEF_PARAM?!");
 			}
 			params = new HashMap();
 			ResultSetMetaData rsmd = rs.getMetaData();
-			for(int col=1, ncols=rsmd.getColumnCount(); col<=ncols; col++) {
+			for (int col=1, ncols=rsmd.getColumnCount(); col<=ncols; col++) {
 				params.put(rsmd.getColumnName(col).toLowerCase(), rs.getString(col));
 			}
 		} finally {
-			if(rs != null) {
+			if (rs != null) {
 				rs.close();
 			}
-			if(stmt != null) {
+			if (stmt != null) {
 				stmt.close();
 			}
 		}
@@ -49,8 +83,10 @@ public class DefaultParameters {
 	 */
 	public String get(String paramName) {
 		String lcParamName = paramName.toLowerCase();
-		if(!params.containsKey(lcParamName)) {
-			throw new IllegalArgumentException("DEF_PARAM version "+params.get("SCHEMA_VERSION")+" doesn't have the parameter "+paramName);
+		if (!params.containsKey(lcParamName)) {
+			throw new IllegalArgumentException(
+                    "DEF_PARAM version " + params.get("SCHEMA_VERSION") +
+                    " doesn't have the parameter " + paramName);
 		}
 		return (String) params.get(paramName.toLowerCase());
 	}
@@ -63,4 +99,9 @@ public class DefaultParameters {
 		return get("mail_server_name");
 	}
 	
+    public Version getPLSchemaVersion() throws VersionFormatException {
+        Version v = new Version();
+        v.setVersion(get("schema_version"));
+        return v;
+    }
 }
