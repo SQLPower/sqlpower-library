199d12b20cfc8a97f8136851001faa04fa2b0be5
matthew <matthew@49f7741a-0836-0410-9614-f5158065ea1e>
2007-03-12 15:23:30 +0000

    moved the browser util demo into the general library to be with the browser util class

---
+++ regress/ca/sqlpower/util/BrowserUtilDemo.java
@@ -0,0 +1,12 @@
+package ca.sqlpower.util;
+
+import java.io.IOException;
+
+import ca.sqlpower.util.BrowserUtil;
+
+public class BrowserUtilDemo {
+    public static void main(String[] args) throws IOException {
+        BrowserUtil.launch("http://www.sqlpower.ca/");
+    }
+
+}
