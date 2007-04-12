d6913ee59c884018d7baf1c5e8637e8dd2762221
ian <ian@49f7741a-0836-0410-9614-f5158065ea1e>
2007-04-12 18:13:47 +0000

    Add a very few known SQLState -> Our Error Type conversions.

--- src/ca/sqlpower/sql/PostgreSQLErrorConverter.java
+++ src/ca/sqlpower/sql/PostgreSQLErrorConverter.java
@@ -2,12 +2,25 @@ package ca.sqlpower.sql;
 
 import java.sql.SQLException;
 
+import org.apache.log4j.Logger;
+
 /**
  * Converts SQLExceptions from PostgreSQL into SQLPower error numbers.
  */
 public class PostgreSQLErrorConverter extends AbstractErrorConverter {
-	
+
+	private final static Logger logger = Logger.getLogger(PostgreSQLErrorConverter.class);
+
 	public int convert(SQLException e) {
+		String state = e.getSQLState();
+
+		logger.debug("SQL STATE = " + state);
+
+		if ("28000".equals(state))
+			return INVALID_LOGON;
+		if ("42703".equals(state))
+			return UNKNOWN_COLUMN;	// postgresql 8.x, proto version 3
+
 		switch (e.getErrorCode()) {
 
 		default:
