e0dd92eea818283aa933d0440f45d5a47098c571
youngthamus <youngthamus@49f7741a-0836-0410-9614-f5158065ea1e>
2007-08-03 20:58:19 +0000

    Got rid of depricated Date constructors to squash warnings.
    Added a few more test cases so that DAILY would not be the only frequency tested.
    
    Added comments to illustrate what each test does.
    
    Note: Used Calendar because it takes more human-readable
    arguements than the Date(long l) constructor.

--- regress/ca/sqlpower/util/RecurrenceTest.java
+++ regress/ca/sqlpower/util/RecurrenceTest.java
@@ -1,43 +1,105 @@
 package ca.sqlpower.util;
 
-import junit.framework.*;
-import ca.sqlpower.util.*;
-import java.util.Date;
+import java.util.Calendar;
+
+import junit.framework.TestCase;
 
 /**
  * Tests the Recurrence class.
  */
 public class RecurrenceTest extends TestCase {
 	public Recurrence r;
+	private Calendar calendar;
+	private Calendar calendar2;
 
 	public RecurrenceTest() {
 	}
 
 	public RecurrenceTest(String name) {
 		super(name);
+		calendar = Calendar.getInstance();
+		calendar2 = Calendar.getInstance();
 	}
 
 	public void setUp() {
 		r = new Recurrence();
-		r.setStartDate(new Date(104, 0, 25, 11, 30, 30));
-	}
-
-	public void testDailySameDay() {
-		r.setFrequency(new Frequency(Frequency.DAILY));
-		assertEquals(r.nextOccurrence(new Date(104, 0, 26, 11, 00)),
-					new Date(104, 0, 26, 11, 30, 30));
-	}
-
-	public void testDailyNextDay() {
-		r.setFrequency(new Frequency(Frequency.DAILY));
-		assertEquals(r.nextOccurrence(new Date(104, 0, 26, 12, 00)),
-					new Date(104, 0, 27, 11, 30, 30));
+		
+		calendar.set(2004, 0, 25, 11, 30, 30); // 2004/01/25 11:30:30
+		r.setStartDate(calendar.getTime());
 	}
 	
+	/**
+	 * Ensures that nextOccurrence(Date baseDate) works
+	 * when the next occurrence is scheduled for later in the
+	 * same day as baseDate
+	 */
+	public void testDailySameDay() {
+		r.setFrequency(new Frequency(Frequency.DAILY));
+		calendar.set(2004, 0, 26, 11, 00); // 2004/01/26 11:00
+		calendar2.set(2004, 0, 26, 11, 30, 30); // 2004/01/26 11:30:30
+		assertEquals(r.nextOccurrence(calendar.getTime()),
+					calendar2.getTime());
+	}
+
+	/**
+	 * Ensures that nextOccurrence(Date baseDate) works
+	 * when the next occurrence is scheduled for the next day
+	 */
+	public void testDailyNextDay() {
+		r.setFrequency(new Frequency(Frequency.DAILY));
+		calendar.set(2004, 0, 26, 12, 00); // 2004/01/26 12:00
+		calendar2.set(2004, 0, 27, 11, 30, 30); // 2004/01/27 11:30:30
+		assertEquals(r.nextOccurrence(calendar.getTime()),     
+				calendar2.getTime());                   
+	}
+	
+	/**
+	 * Ensures that nextOccurrence(Date baseDate) works
+	 * when the next occurrence is scheduled for the next day
+	 * at the same time of day
+	 */
 	public void testDailyOnBoundary() {
 		r.setFrequency(new Frequency(Frequency.DAILY));
-		assertEquals(r.nextOccurrence(new Date(104, 0, 27, 11, 30, 30)),
-					new Date(104, 0, 28, 11, 30, 30));
+		calendar.set(2004, 0, 27, 11, 30, 30); // 2004/01/27 11:30:30
+		calendar2.set(2004, 0, 28, 11, 30, 30); // 2004/01/28 11:30:30
+		assertEquals(r.nextOccurrence(calendar.getTime()),
+					calendar2.getTime());
+	}
+
+	/**
+	 * Ensures that nextOccurrence(Date baseDate) works
+	 * when the next occurrence is scheduled for the same week
+	 */
+	public void testWeeklySameWeek() {
+		r.setFrequency(new Frequency(Frequency.WEEKLY));
+		calendar.set(2004, 0, 24, 12, 00); // 2004/01/24 12:00
+		calendar2.set(2004, 0, 25, 11, 30, 30); // 2004/01/25 11:30:30
+		assertEquals(r.nextOccurrence(calendar.getTime()),     
+				calendar2.getTime());                   
+	}
+	
+	/**
+	 * Ensures that nextOccurrence(Date baseDate) works
+	 * when the next occurrence is scheduled for the same month
+	 */
+	public void testMonthlySameMonth() {
+		r.setFrequency(new Frequency(Frequency.MONTHLY));
+		calendar.set(2004, 0, 24, 12, 00); // 2004/01/24 12:00
+		calendar2.set(2004, 0, 25, 11, 30, 30); // 2004/01/25 11:30:30
+		assertEquals(r.nextOccurrence(calendar.getTime()),     
+				calendar2.getTime());                   
+	}
+
+	/**
+	 * Ensures that nextOccurrence(Date baseDate) works
+	 * when the next occurrence is scheduled for the same year
+	 */
+	public void testYearlySameYear() {
+		r.setFrequency(new Frequency(Frequency.YEARLY));
+		calendar.set(2004, 0, 24, 12, 00); // 2004/01/24 12:00
+		calendar2.set(2004, 0, 25, 11, 30, 30); // 2004/01/25 11:30:30
+		assertEquals(r.nextOccurrence(calendar.getTime()),     
+				calendar2.getTime());                   
 	}
 
 }
