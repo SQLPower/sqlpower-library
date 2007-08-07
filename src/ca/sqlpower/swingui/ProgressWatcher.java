1b0cb4e7f2d21bf402b71ed00df5aef358ca7c86
mo.jeff <mo.jeff@49f7741a-0836-0410-9614-f5158065ea1e>
2007-08-07 16:35:44 +0000

    Refactored the ProgressWatcher in the following ways: * Modified the ProgressWatcher behaviour so that the constructor no longer starts the ProgressWatcher. Instead, you must call the start() method after to start it. This makes the constructor behaviour more intuitive (it just constructs the ProgressWatcher). For the old ProgressWatcher behaviour (construct and start), there are new static watchProgress() methods for each of the constructors.
    * Added hideLabelWhenFinished and hideProgressBarWhenFinished
    booleans, and set the default to false. If you want the progress bar or label to be hidden once the Monitorable is finished, use the setter methods.

--- src/ca/sqlpower/swingui/ProgressWatcher.java
+++ src/ca/sqlpower/swingui/ProgressWatcher.java
@@ -33,9 +33,6 @@ package ca.sqlpower.swingui;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
 
 import javax.swing.JLabel;
 import javax.swing.JProgressBar;
@@ -44,43 +41,151 @@ import javax.swing.Timer;
 
 import org.apache.log4j.Logger;
 
-import ca.sqlpower.swingui.event.TaskTerminationEvent;
-import ca.sqlpower.swingui.event.TaskTerminationListener;
 import ca.sqlpower.util.Monitorable;
 
+/**
+ * This class updates a JProgressBar based on the progress of a given Monitorable
+ * object. It places itself as the ActionListener within a Timer object, and uses
+ * it to periodically update the progress bar. The ProgressWatcher also allows you to
+ * set whether or not 
+ */
 public class ProgressWatcher implements ActionListener {
     private JProgressBar bar = null;
+    private boolean hideProgressBarWhenFinished = false;
     private ProgressMonitor pm = null;
     private Monitorable monitorable = null;
     private JLabel label = null;
+    private boolean hideLabelWhenFinished = false;
     private Timer timer;
 
     private static final Logger logger = Logger.getLogger(ProgressWatcher.class);
 
-    public ProgressWatcher() {
-    }
-
+    /**
+     * Create a ProgressWatcher with the given progress bar and Monitorable.
+     * It will leave the progress bar label blank. The label and progress bar will 
+     * remain visible after the monitorable is finished by default.  
+     * <p>
+     * The progress bar will not start up at this point. You should either use the static method
+     * {@link #watchProgress(JProgressBar, Monitorable)} or call the {@link #start()}
+     * method after calling the constructor to start the progress bar monitor.
+     * 
+     * @param bar The JProgressBar that is tracking the progress of the monitorable
+     * @param monitorable The monitorable object that the ProgressWatcher monitors 
+     */
     public ProgressWatcher(JProgressBar bar, Monitorable monitorable) {
         this (bar,monitorable,null);
     }
 
+    /**
+     * Create a ProgressWatcher with the given progress bar and Monitorable, and set the 
+     * progress bar label to the given JLabel. The label and progress bar will remain visible
+     * after the monitorable is finished by default.  
+     * <p>
+     * The progress bar will not start up at this point. You should either use the static method
+     * {@link #watchProgress(JProgressBar, Monitorable, JLabel))} or call the {@link #start()}
+     * method after calling the constructor to start the progress bar monitor.
+     * 
+     * @param bar The JProgressBar that is tracking the progress of the monitorable
+     * @param monitorable The monitorable object that the ProgressWatcher monitors
+     * @param label The label you want to display on the progress bar 
+     */
     public ProgressWatcher(JProgressBar bar, Monitorable monitorable, JLabel label) {
-        this();
         this.bar = bar;
         this.monitorable = monitorable;
         this.label = label;
-        timer = new Timer(50, this);
-        timer.start();
     }
 
-    public ProgressWatcher (ProgressMonitor pm, Monitorable monitorable) {
-        this();
+    /**
+     * Create a ProgressWatcher with the given ProgressMonitor and Monitorable.
+     * It will leave the progress bar label blank. The label and progress bar will 
+     * remain visible after the monitorable is finished by default. 
+     * <p>
+     * The progress bar will not start up at this point. You should either use the static method
+     * {@link #watchProgress(JProgressBar, Monitorable)} or call the {@link #start()}
+     * method after calling the constructor to start the progress bar monitor.
+     * 
+     * @param bar The JProgressBar that is tracking the progress of the monitorable
+     * @param monitorable The monitorable object that the ProgressWatcher monitors 
+     */
+    public ProgressWatcher(ProgressMonitor pm, Monitorable monitorable) {
         this.pm = pm;
         this.monitorable = monitorable;
-        timer = new Timer(50, this);
-        timer.start();
     }
 
+    /**
+     * Start the progress bar watcher to update the progress bar based 
+     * on the progress of the given monitorable object. The progress bar
+     * is set to update every 50 ms.
+     */
+    public void start() {
+    	timer = new Timer(50, this);
+    	timer.start();
+    }
+
+    /**
+     * Creates a ProgressWatcher with the given JProgressBar and Monitorable using the 
+     * {@link #ProgressWatcher(JProgressBar, Monitorable)} constructor and starts it immediately.
+     * <p>
+     * Also, the progress bar and label will remain visible once the monitorable is finished.
+     * If you prefer to have the progress bar and label to be hidden after the monitorable is finished,
+     * then use the {@link #ProgressWatcher(JProgressBar, Monitorable)} constructor, followed by
+     * {@link #setHideLabelWhenFinished(boolean)} and/or {@link #setHideProgressBarWhenFinished(boolean)}
+     * method calls to hide the label and/or progress bar after the Monitorable is finished, and then 
+     * call {@link #start()} to start the ProgressWatcher.
+     * 
+     * @param bar The JProgressBar that will be used to track the progress of the Monitorable object
+     * @param monitorable The Monitorable object that will be monitored to update the progress bar
+     */
+    public static void watchProgress(JProgressBar bar, Monitorable monitorable) {
+    	ProgressWatcher watcher = new ProgressWatcher(bar, monitorable);
+    	watcher.start();
+    }
+    
+    /**
+     * Creates a ProgressWatcher with the given JProgressBar, Monitorable, and JLabel using the 
+     * {@link #ProgressWatcher(JProgressBar, Monitorable, JLabel)} constructor and starts it immediately.
+     * 
+     * Also, the progress bar and label will remain visible once the monitorable is finished.
+     * If you prefer to have the progress bar and label to be hidden after the monitorable is finished,
+     * then use the {@link #ProgressWatcher(JProgressBar, Monitorable, JLabel)} constructor, followed by
+     * {@link #setHideLabelWhenFinished(boolean)} and/or {@link #setHideProgressBarWhenFinished(boolean)}
+     * method calls to hide the label and/or progress bar after the Monitorable is finished, and then 
+     * call {@link #start()} to start the ProgressWatcher.
+     * 
+     * @param bar The JProgressBar that will be used to track the progress of the Monitorable object
+     * @param monitorable The Monitorable object that will be monitored to update the progress bar
+     * @param label The label that will be displayed next to the progress bar
+     */
+    public static void watchProgress(JProgressBar bar, Monitorable monitorable, JLabel label) {
+    	ProgressWatcher watcher = new ProgressWatcher(bar, monitorable, label);
+    	watcher.start();
+    }
+    
+    /**
+     * Creates a ProgressWatcher with the given ProgressMonitor and Monitorable using the 
+     * {@link #ProgressWatcher(ProgressMonitor, Monitorable)} constructor and starts it immediately.
+     *
+     * Also, the progress bar and label will remain visible once the monitorable is finished.
+     * If you prefer to have the progress bar and label to be hidden after the monitorable is finished,
+     * then use the {@link #ProgressWatcher(ProgressMonitor, Monitorable)} constructor, followed by
+     * {@link #setHideLabelWhenFinished(boolean)} and/or {@link #setHideProgressBarWhenFinished(boolean)}
+     * method calls to hide the label and/or progress bar after the Monitorable is finished, and then 
+     * call {@link #start()} to start the ProgressWatcher.
+     * 
+     * @param bar The ProgressMonitor that will used to monitor the progress of the Monitorable object
+     * @param monitorable The Monitorable object that will be monitored to update the progress bar
+     */
+    public static void watchProgress(ProgressMonitor pm, Monitorable monitorable) {
+    	ProgressWatcher watcher = new ProgressWatcher(pm, monitorable);
+    	watcher.start();
+    }
+    
+    /**
+     * The method that is run when the timer thread notifies the ProgressWatcher. It 
+     * updates the progress bar based on the status of the monitorable object. When the monitorable
+     * is finished, it will hide the progress bar if {@link #hideProgressBarWhenFinished} is set to
+     * true and it will hide the label if {@link #hideLabelWhenFinished} is set to true.
+     */
     public void actionPerformed(ActionEvent evt) {
         // update the progress bar
         logger.debug("updating progress bar...");
@@ -96,9 +201,9 @@ public class ProgressWatcher implements ActionListener {
                 }
                 bar.setVisible(true);
             }
-            if (monitorable.isFinished()){
+            if (monitorable.isFinished() && hideProgressBarWhenFinished){
                 bar.setVisible(false);
-            }
+            } 
         }
 
         if (label != null) {
@@ -117,11 +222,12 @@ public class ProgressWatcher implements ActionListener {
         }
         logger.debug("monitorable.isFinished():" + monitorable.isFinished());
         if (monitorable.isFinished()) {
-            if (label != null) {
+            if (label != null && hideLabelWhenFinished) {
                 label.setText("");
             }
             if (bar != null) {
                 bar.setValue(0);
+                bar.setIndeterminate(false);
             }
             if (pm != null) {
                 logger.debug("pm done, max was: " + pm.getMaximum());
@@ -133,5 +239,39 @@ public class ProgressWatcher implements ActionListener {
             logger.debug("did the timer thread stop???");
         }
     }
+
+    /**
+     * Returns true if the progress bar will be hidden once the monitorable is finished.
+     * Otherwise returns false, in that case the progress bar will remain and the value set to 0
+     * when finished. The default value is false.
+     */
+	public boolean isHideProgressBarWhenFinished() {
+		return hideProgressBarWhenFinished;
+	}
+
+    /**
+     * Set if the progress bar will be hidden once the monitorable is finished.
+     * The default value is false.
+     */
+	public void setHideProgressBarWhenFinished(boolean hideProgressBarWhenFinished) {
+		this.hideProgressBarWhenFinished = hideProgressBarWhenFinished;
+	}
+
+    /**
+     * Returns true if the label will be hidden once the monitorable is finished.
+     * Otherwise returns false, in that case the label will remain when finished. 
+     * The default value is false.
+     */
+	public boolean isHideLabelWhenFinished() {
+		return hideLabelWhenFinished;
+	}
+
+    /**
+     * Set if the label will be hidden once the monitorable is finished.
+     * The default value is false.
+     */
+	public void setHideLabelWhenFinished(boolean hideLabelWhenFinished) {
+		this.hideLabelWhenFinished = hideLabelWhenFinished;
+	}
 }
 	
