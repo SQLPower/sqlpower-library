4b94e106c4af3371f59f3ab464bcf80a5b639ba7
jeffrey <jeffrey@sqlpower.ca>
2007-09-10 16:22:02 +0000

    Moved the BrowseFileAction from inside the match engine panel of the MatchMaker.  It's totally generic, and we need to reuse it in another class.

---
+++ src/ca/sqlpower/swingui/BrowseFileAction.java
@@ -0,0 +1,50 @@
+package ca.sqlpower.swingui;
+
+import java.awt.Component;
+import java.awt.event.ActionEvent;
+import java.io.File;
+
+import javax.swing.AbstractAction;
+import javax.swing.JFileChooser;
+import javax.swing.JTextField;
+
+/**
+ * An action for popping up a file chooser to edit a path name in a JTextField.
+ * This action can be used to provide the typical "..." button found beside so
+ * many fine text fields these days.
+ */
+public class BrowseFileAction extends AbstractAction {
+
+	/**
+	 * The component that should be the parent component of the file
+	 * chooser dialog.
+	 */
+	private final Component parentComponent;
+	
+	/**
+	 * The field to use for the file path.
+	 */
+	private final JTextField field;
+
+	public BrowseFileAction(Component parentComponent, JTextField field) {
+		super("...");
+		if (field == null) throw new NullPointerException("You have to specify a field");
+		this.parentComponent = parentComponent;
+		this.field = field;
+	}
+	
+	public void actionPerformed(ActionEvent e) {
+		JFileChooser fileChooser = new JFileChooser();
+		if (field.getText() != null && field.getText().length() > 0) {
+			File defaultFile = new File(field.getText());
+			fileChooser.setCurrentDirectory(defaultFile);
+			fileChooser.setSelectedFile(defaultFile);
+		}
+		int returnVal = fileChooser.showOpenDialog(parentComponent);
+		if (returnVal == JFileChooser.APPROVE_OPTION) {
+			final File file = fileChooser.getSelectedFile();
+			field.setText(file.getPath());
+		}
+	}
+	
+}
