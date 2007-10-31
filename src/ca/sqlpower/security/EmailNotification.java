db6a87fd1dfd6ad9ed838b69f5420902fe1dd41e
Kevin1219 <Kevin1219@49f7741a-0836-0410-9614-f5158065ea1e>
2007-10-31 21:10:04 +0000

    -Added the mailapi that helps with sending emails -Copied and modified the Email.java class from the website eclipse project. It now keeps a list of recipients and sends the email to all of them. -Modified EmailNotification to prepare for implementing sending emails in matchmaker. Re-wrote all the check status methods as they didn't work before. Now there are status checks for each type of status (red, yellow, green) and based on user, user and group, group. Added a new method to find all the users that have been indicated for emails of a certain status of a given DatabaseObject. This method returns a list of MailRecipient which holds the name and email of the user.
    TODO: 
    Change the finding mail recipients method so that it finds users who are in groups that have been indicated for receiving emails also.
    TODO:

--- src/ca/sqlpower/security/EmailNotification.java
+++ src/ca/sqlpower/security/EmailNotification.java
@@ -32,14 +32,21 @@
 
 package ca.sqlpower.security;
 
-import java.sql.*;
-import java.util.*;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
-import ca.sqlpower.sql.*;
+import ca.sqlpower.sql.DatabaseObject;
+import ca.sqlpower.sql.SQL;
+import ca.sqlpower.util.UnknownFreqCodeException;
 
 /**
  * The EmailNotification class answers questions about email
- * notification for users or groups on spe*cific objects, and allows
+ * notification for users or groups on specific objects, and allows
  * updating of notification preferences.
  * 
  * @author Gillian Mereweather
@@ -49,6 +56,24 @@ import ca.sqlpower.sql.*;
 public class EmailNotification implements java.io.Serializable {
 
 	/**
+	 * The name of the column indicating whether to send emails at red status
+	 * on either pl_user_notification or pl_group_notification.
+	 */
+	private static final String RED_STATUS = "EMAIL_RED_IND";
+	
+	/**
+	 * The name of the column indicating whether to send emails at yellow status
+	 * on either pl_user_notification or pl_group_notification.
+	 */
+	private static final String YELLOW_STATUS = "EMAIL_YELLOW_IND";
+	
+	/**
+	 * The name of the column indicating whether to send emails at green status
+	 * on either pl_user_notification or pl_group_notification.
+	 */
+	private static final String GREEN_STATUS = "EMAIL_GREEN_IND";
+
+	/**
 	 * Stores a pl_user_notification record in the database.  
 	 * This could update an existing record or insert a new one.
 	 *
@@ -56,15 +81,15 @@ public class EmailNotification implements java.io.Serializable {
 	 *
 	 * @param sm The current logged-in user's security manager.
 	 */
-    public static void setPref(Connection con,
-							   PLSecurityManager sm,
-							   PLUser notifyUser,
-							   DatabaseObject notifyAbout,
-							   String viewKpi,
-							   String emailRed,
-							   String emailYellow,
-							   String emailGreen)
-		throws SQLException, PLSecurityException {
+	public static void setPref(Connection con,
+			PLSecurityManager sm,
+			PLUser notifyUser,
+			DatabaseObject notifyAbout,
+			String viewKpi,
+			String emailRed,
+			String emailYellow,
+			String emailGreen)
+	throws SQLException, PLSecurityException {
 
 		sm.checkModify(con, notifyUser);
 		setPref(con, notifyUser.getUserId(), true, notifyAbout, 
@@ -79,15 +104,15 @@ public class EmailNotification implements java.io.Serializable {
 	 *
 	 * @param sm The current logged-in user's security manager.
 	 */
-    public static void setPref(Connection con,
-							   PLSecurityManager sm,
-							   PLGroup notifyGroup,
-							   DatabaseObject notifyAbout,
-							   String viewKpi,
-							   String emailRed,
-							   String emailYellow,
-							   String emailGreen)
-		throws SQLException, PLSecurityException {
+	public static void setPref(Connection con,
+			PLSecurityManager sm,
+			PLGroup notifyGroup,
+			DatabaseObject notifyAbout,
+			String viewKpi,
+			String emailRed,
+			String emailYellow,
+			String emailGreen)
+	throws SQLException, PLSecurityException {
 
 		sm.checkModify(con, notifyGroup);
 		setPref(con, notifyGroup.getGroupName(), false, notifyAbout, 
@@ -102,16 +127,16 @@ public class EmailNotification implements java.io.Serializable {
 	 * if this is for group notifications.
 	 */
 	protected static void setPref(Connection con,
-								  String notifyName,
-								  boolean nameIsUser,
-								  DatabaseObject notifyAbout,
-								  String viewKpi,
-								  String emailRed,
-								  String emailYellow,
-								  String emailGreen)
-		throws SQLException {
+			String notifyName,
+			boolean nameIsUser,
+			DatabaseObject notifyAbout,
+			String viewKpi,
+			String emailRed,
+			String emailYellow,
+			String emailGreen)
+	throws SQLException {
 
-        Statement stmt = null;
+		Statement stmt = null;
 		ResultSet rs = null;
 		boolean bFirst=true;
 		try {
@@ -129,7 +154,7 @@ public class EmailNotification implements java.io.Serializable {
 			sql.append(SQL.quote(notifyName));
 			sql.append(" AND object_type=").append(SQL.quote(notifyAbout.getObjectType()));
 			sql.append(" AND object_name=").append(SQL.quote(notifyAbout.getObjectName()));
-			
+
 			rs = stmt.executeQuery(sql.toString());
 
 			sql.setLength(0);
@@ -213,67 +238,127 @@ public class EmailNotification implements java.io.Serializable {
 		}
 	}
 
-    /**
-	 * Returns the email notification preferences for the given user
-	 * on the given object.  Does nothing about groups that this user
-	 * may belong to.
+	/**
+	 * Returns the email notification preference of green status for
+	 * the given user on the given object.  Does nothing about groups
+	 * that this user may belong to.
 	 */
-	public static boolean checkUserStatus(Connection con, 
-									  PLUser user,
-									  DatabaseObject dbObj,
-									  String runStatus) 
-	throws SQLException {
-		return checkStatus(con, user.getUserId(), true, dbObj, runStatus);
+	public static boolean checkUserGreenStatus(Connection con, 
+			PLUser user, DatabaseObject dbObj) throws SQLException {
+		return checkStatus(con, user.getUserId(), true, dbObj, GREEN_STATUS);
 	}
 
-    /**
-	 * Returns the email notification preferences for the given user
-	 * on the given object, including the additional checks for the
-	 * groups the user belongs to.
+	/**
+	 * Returns the email notification preference of yellow status for
+	 * the given user on the given object.  Does nothing about groups
+	 * that this user may belong to.
 	 */
-	public static boolean checkStatus(Connection con, 
-									  PLUser user,
-									  DatabaseObject dbObj,
-									  String runStatus) 
-	throws SQLException {
-		if (checkStatus(con, user.getUserId(), true, dbObj, runStatus)) {
+	public static boolean checkUserYellowStatus(Connection con, 
+			PLUser user, DatabaseObject dbObj) throws SQLException {
+		return checkStatus(con, user.getUserId(), true, dbObj, YELLOW_STATUS);
+	}
+
+	/**
+	 * Returns the email notification preference of red status for
+	 * the given user on the given object.  Does nothing about groups
+	 * that this user may belong to.
+	 */
+	public static boolean checkUserRedStatus(Connection con, 
+			PLUser user, DatabaseObject dbObj) throws SQLException {
+		return checkStatus(con, user.getUserId(), true, dbObj, RED_STATUS);
+	}
+
+	/**
+	 * Returns the email notification preferences for the 
+	 * given user on the given object, including the
+	 * additional checks for the groups the user belongs to.
+	 */
+	private static boolean checkUserAndGroupStatus(Connection con, 
+			PLUser user, DatabaseObject dbObj, String statusType) throws SQLException {
+		if (checkStatus(con, user.getUserId(), true, dbObj, statusType)) {
 			return true;
 		}
 		Iterator groups = user.getGroups(con).iterator();
 		while (groups.hasNext()) {
 			PLGroup g = (PLGroup) groups.next();
-			if (checkStatus(con, g.getGroupName(), false, dbObj, runStatus)) {
+			if (checkStatus(con, g.getGroupName(), false, dbObj, statusType)) {
 				return true;
 			}
 		}
 		return false;
 	}
 
-    /**
-	 * Returns the email notification preferences for the given group
-	 * on the given object.
+	/**
+	 * Returns the email notification preferences of green status 
+	 * for the given user on the given object, including the
+	 * additional checks for the groups the user belongs to.
 	 */
-	public static boolean checkStatus(Connection con, 
-									  PLGroup group,
-									  DatabaseObject dbObj,
-									  String runStatus) 
-	throws SQLException {
-		return checkStatus(con, group.getGroupName(), false, dbObj, runStatus);
+	public static boolean checkUserAndGroupGreenStatus(Connection con, 
+			PLUser user, DatabaseObject dbObj) throws SQLException {
+		return checkUserAndGroupStatus(con, user, dbObj, GREEN_STATUS);
+	}
+
+	/**
+	 * Returns the email notification preferences of yellow status 
+	 * for the given user on the given object, including the
+	 * additional checks for the groups the user belongs to.
+	 */
+	public static boolean checkUserAndGroupYellowStatus(Connection con, 
+			PLUser user, DatabaseObject dbObj) throws SQLException {
+		return checkUserAndGroupStatus(con, user, dbObj, YELLOW_STATUS);
+	}
+
+	/**
+	 * Returns the email notification preferences of red status 
+	 * for the given user on the given object, including the
+	 * additional checks for the groups the user belongs to.
+	 */
+	public static boolean checkUserAndGroupRedStatus(Connection con, 
+			PLUser user, DatabaseObject dbObj) throws SQLException {
+		return checkUserAndGroupStatus(con, user, dbObj, RED_STATUS);
+	}
+
+
+	/**
+	 * Returns the email notification preferences of green status 
+	 * for the given group on the given object.
+	 */
+	public static boolean checkGroupGreenStatus(Connection con, 
+			PLGroup group, DatabaseObject dbObj) throws SQLException {
+		return checkStatus(con, group.getGroupName(), false, dbObj, GREEN_STATUS);
+	}
+
+	/**
+	 * Returns the email notification preferences of green status 
+	 * for the given group on the given object.
+	 */
+	public static boolean checkGroupYellowStatus(Connection con, 
+			PLGroup group, DatabaseObject dbObj) throws SQLException {
+		return checkStatus(con, group.getGroupName(), false, dbObj, YELLOW_STATUS);
+	}
+
+	/**
+	 * Returns the email notification preferences of green status 
+	 * for the given group on the given object.
+	 */
+	public static boolean checkGroupRedStatus(Connection con, 
+			PLGroup group, DatabaseObject dbObj) throws SQLException {
+		return checkStatus(con, group.getGroupName(), false, dbObj, RED_STATUS);
 	}
 
 	/**
 	 * Used internally by the user and group versions of checkStatus.
 	 */
-	protected static boolean checkStatus(Connection con,
-										 String name,
-										 boolean nameIsUser,
-										 DatabaseObject dbObj,
-										 String runStatus) throws SQLException {
-        Statement stmt = null;
-        try {
+	private static boolean checkStatus(Connection con,
+			String name,
+			boolean nameIsUser,
+			DatabaseObject dbObj,
+			String statusType) throws SQLException {
+		Statement stmt = null;
+		try {
 			StringBuffer sql = new StringBuffer();
 
-			sql.append("SELECT run_status");
+			sql.append("SELECT " + statusType);
 
 			if(nameIsUser){
 				sql.append(" FROM pl_user_notification");
@@ -285,24 +370,92 @@ public class EmailNotification implements java.io.Serializable {
 			sql.append(" AND object_type=").append(SQL.quote(dbObj.getObjectType()));
 			sql.append(" AND object_name=").append(SQL.quote(dbObj.getObjectName()));
 
-            stmt = con.createStatement();
-            ResultSet rs = stmt.executeQuery(sql.toString());
+			stmt = con.createStatement();
+			ResultSet rs = stmt.executeQuery(sql.toString());
 			while(rs.next()) {
 				String prefStatus = rs.getString(1);
-				if (prefStatus != null && prefStatus.equals(runStatus)) {
+				if (prefStatus != null && prefStatus.equals("Y")) {
 					return true;
 				}
 			}
 
 			// There wasn't a matching entry in the table
-            return false;
+			return false;
+
+		} finally {
+			if(stmt != null) {
+				stmt.close();
+			}
+		}
+	}
+	
+	/**
+	 * Internal method that returns a list of EmailRecipients containing
+	 * the email and name of users who are indicated for receiving emails
+	 * of given status of given DatabaseObject. This currently does not
+	 * check for groups that users could belong to. 
+	 */
+	private static List<EmailRecipient> findEmailRecipients(Connection con, DatabaseObject dbObj,
+			String statusType) throws SQLException {
+		List<EmailRecipient> emailRecipients = new ArrayList<EmailRecipient>();
+		
+        Statement stmt = null;
+        try {
+			StringBuffer sql = new StringBuffer();
+			sql.append("SELECT user_name, email_address FROM pl_user in (");
+			sql.append("SELECT user_id");
+			sql.append(" FROM pl_user_notification");
+			
+			sql.append(" WHERE " + statusType + " = " + SQL.quote("Y"));
+			sql.append(" AND object_type = " + SQL.quote(dbObj.getObjectType()));
+			sql.append(" AND object_name = " + SQL.quote(dbObj.getObjectName()) + ")");
+
+            stmt = con.createStatement();
+            ResultSet rs = stmt.executeQuery(sql.toString());
+			
+			while(rs.next()) {
+				EmailRecipient er = new EmailRecipient(rs.getString(1), rs.getString(2));
+				emailRecipients.add(er);
+			};
 
         } finally {
             if(stmt != null) {
                 stmt.close();
             }
         }
-    }
+		
+		return emailRecipients;
+	}
+
+	/**
+	 * Returns a list of EmailRecipients to whom emails of green status
+	 * of given DatabaseObject should be sent to. This does not check for
+	 * groups that users could belong to.
+	 */
+	public static List<EmailRecipient> findGreenEmailRecipients(Connection con, DatabaseObject dbObj) 
+			throws UnknownFreqCodeException, PLSecurityException, SQLException {
+		return findEmailRecipients(con, dbObj, GREEN_STATUS) ;
+	}
+	
+	/**
+	 * Returns a list of EmailRecipients to whom emails of yellow status
+	 * of given DatabaseObject should be sent to. This does not check for
+	 * groups that users could belong to.
+	 */
+	public static List<EmailRecipient> findYellowEmailRecipients(Connection con, DatabaseObject dbObj) 
+			throws UnknownFreqCodeException, PLSecurityException, SQLException {
+		return findEmailRecipients(con, dbObj, YELLOW_STATUS) ;
+	}
+
+	/**
+	 * Returns a list of EmailRecipients to whom emails of red status
+	 * of given DatabaseObject should be sent to. This does not check for
+	 * groups that users could belong to.
+	 */
+	public static List<EmailRecipient> findRedEmailRecipients(Connection con, DatabaseObject dbObj) 
+			throws UnknownFreqCodeException, PLSecurityException, SQLException {
+		return findEmailRecipients(con, dbObj, RED_STATUS) ;
+	}
 
 	/**
 	 * This method is normally called from {@link
@@ -328,8 +481,8 @@ public class EmailNotification implements java.io.Serializable {
 	 * @param obj The database object we're evicting.
 	 */
 	public static void deleteDatabaseObject(Connection con, PLSecurityManager sm,
-											DatabaseObject obj)
-		throws PLSecurityException, SQLException {
+			DatabaseObject obj)
+	throws PLSecurityException, SQLException {
 
 		sm.checkDelete(con, obj);
 
@@ -389,8 +542,8 @@ public class EmailNotification implements java.io.Serializable {
 	 * @param newName the new name we are assigning to the object.
 	 */
 	public static void renameDatabaseObject(Connection con, PLSecurityManager sm,
-											DatabaseObject obj, String newName)
-		throws PLSecurityException, SQLException {
+			DatabaseObject obj, String newName)
+	throws PLSecurityException, SQLException {
 
 		sm.checkModify(con, obj);
 
@@ -422,4 +575,38 @@ public class EmailNotification implements java.io.Serializable {
 			}
 		}
 	}
+	
+	
+	/**
+	 * A simple inner class to hold the name and email of an
+	 * user that has been indicated as an email recipient
+	 */
+	public static class EmailRecipient {
+		
+		private String name;
+		private String email;
+		
+		public EmailRecipient(String name, String email) {
+			this.name = name;
+			this.email = email;
+		}
+		
+		public String getName() {
+			return name;
+		}
+		
+		public void setName(String name) {
+			this.name = name;
+		}
+		
+		public String getEmail() {
+			return email;
+		}
+		
+		public void setEmail(String email) {
+			this.email = email;
+		}
+		
+	}
 }
+
---
+++ src/ca/sqlpower/util/Email.java
@@ -0,0 +1,166 @@
+package ca.sqlpower.util;
+
+import java.io.UnsupportedEncodingException;
+import java.util.List;
+import java.util.Properties;
+
+import javax.mail.Address;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
+/**
+ * This class is used as a convenient wrapper for the JavaMail stuff.
+ * The API is simpler because a lot of the default boilerplate setup
+ * is the same for all messages we send.
+ * 
+ * @author Dan Fraser
+ */
+public class Email {
+
+	private String smtpHost;
+	private String smtpLocalhost;
+	
+    /**
+     * Creates a new email object with all the default settings.
+     */
+    public Email(String smtpHost, String smtpLocalhost) {
+    	this.smtpHost = smtpHost;
+    	this.smtpLocalhost = smtpLocalhost;
+    }
+    
+    /**
+     * The Internet addresses of the addressees.
+     */
+    private List<Address> toAddresses;
+    
+    /**
+     * The human-readable name of the sender.
+     */
+    private String fromName;
+    
+    /**
+     * The Internet email address of the sender.
+     */
+    private String fromEmail;
+    
+    /**
+     * The subject line for the email.  Should only contain ASCII characters
+     * (no fancy accented characters for you!).
+     */
+    private String emailSubject;
+    
+    /**
+     * The body of the email.
+     */
+    private String emailBody;
+
+    /**
+     * Sends this email using the current settings.  All settings are required,
+     * so don't go skimping on setXXX() calls before calling this!
+     * 
+     * @throws javax.mail.MessagingException if the message could not
+     * be sent.  this is an unusual condition for the website and
+     * probably means that the mail server is down or something.
+     * @throws UnsupportedEncodingException 
+     */
+    public void sendMessage() throws MessagingException {
+        Properties props = new Properties();
+        props.put("mail.smtp.host", smtpHost);
+        props.put("mail.smtp.localhost", smtpLocalhost);
+        Session mailSession = Session.getInstance(props, null);
+        MimeMessage message = new MimeMessage(mailSession);
+        Address[] tempAddresses = new Address[toAddresses.size()];
+        try {
+            message.setFrom(new InternetAddress(fromEmail, fromName));
+            message.addRecipients(Message.RecipientType.TO, toAddresses.toArray(tempAddresses));
+        } catch (java.io.UnsupportedEncodingException e) {
+            throw new AssertionError(e); // this should never ever happen
+        }
+        message.setSubject(emailSubject);
+        message.setText(emailBody);
+        Transport.send(message);
+    }
+
+    public String getEmailBody() {
+        return emailBody;
+    }
+
+    public void setEmailBody(String emailBody) {
+        this.emailBody = emailBody;
+    }
+
+    public String getEmailSubject() {
+        return emailSubject;
+    }
+
+    public void setEmailSubject(String emailSubject) {
+        this.emailSubject = emailSubject;
+    }
+
+    public String getFromEmail() {
+        return fromEmail;
+    }
+
+    public void setFromEmail(String fromEmail) {
+        this.fromEmail = fromEmail;
+    }
+
+    public String getFromName() {
+        return fromName;
+    }
+
+    public void setFromName(String fromName) {
+        this.fromName = fromName;
+    }
+
+    /** 
+     * Returns a String representation of all the
+     * recipients' email addresses. 
+     */
+    public String getToAddresses() {
+    	StringBuilder result = new StringBuilder();
+    	boolean first = true;
+    	for (Address addr : toAddresses) {
+    		if (!first) {
+    			result.append(", ");
+    		}
+    		result.append("<" + ((InternetAddress) addr).getPersonal() + "> ");
+    		result.append(((InternetAddress) addr).getAddress());
+    	}
+    	return result.toString();
+    }
+    
+    public void setToAddresses(List<Address> toAddresses) {
+    	this.toAddresses = toAddresses;
+    }
+    
+    /**
+     * Adds a recipient to the email. 
+     */
+    public void addToAddress(String toEmail, String toName) 
+    		throws UnsupportedEncodingException {
+    	toAddresses.add(new InternetAddress(toEmail, toName));
+    }
+    
+    public void removeToAddress(String toEmail, String toName) 
+    		throws UnsupportedEncodingException {
+    	toAddresses.remove(new InternetAddress(toEmail, toName));
+    }
+    
+    /**
+     * Returns this email in normal email format (headers, then a blank line then the body).
+     */
+    @Override
+    public String toString() {
+        return
+            "From: <"+getFromName()+"> "+getFromEmail()+"\r\n"+
+            "To: <"+getToAddresses()+"\r\n"+
+            "Subject: "+getEmailSubject()+"\r\n"+
+            "\r\n"+
+            getEmailBody()+"\r\n";
+    }
+}
