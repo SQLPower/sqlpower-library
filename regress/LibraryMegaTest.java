b7876d008046ead53a951f8e7f8897ed70ec620f
jeffrey <jeffrey@sqlpower.ca>
2008-04-24 22:31:59 +0000

    LibraryMegaTest was not updated with the graph and util.reservoir tests. Additionally, util.reservoir.JDBCReservoirDataSourceTest has been failing, but one wouldn't know from running the LibraryMegaTest because it didn't include that test.

--- regress/LibraryMegaTest.java
+++ regress/LibraryMegaTest.java
@@ -1,3 +1,4 @@
+import ca.sqlpower.util.reservoir.AllTests;
 import junit.framework.Test;
 import junit.framework.TestSuite;
 
@@ -12,10 +13,12 @@ public class LibraryMegaTest {
     public static Test suite() {
         TestSuite suite = new TestSuite("Test for default package");
         //$JUnit-BEGIN$
+        suite.addTest(ca.sqlpower.graph.AllTests.suite());
         suite.addTest(ca.sqlpower.sql.AllTests.suite());
         suite.addTest(ca.sqlpower.swingui.AllTests.suite());
         suite.addTest(ca.sqlpower.swingui.table.AllTests.suite());
         suite.addTest(ca.sqlpower.util.AllTests.suite());
+        suite.addTest(ca.sqlpower.util.reservoir.AllTests.suite());
         suite.addTest(ca.sqlpower.validation.AllTests.suite());
         suite.addTest(ca.sqlpower.validation.swingui.AllTests.suite());
         suite.addTest(ca.sqlpower.xml.AllTests.suite());
