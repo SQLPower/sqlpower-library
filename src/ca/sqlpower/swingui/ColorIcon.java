d1ad34444d5a43fd7f8b897ab5d5a3333435b4cc
Kevin1219 <Kevin1219@49f7741a-0836-0410-9614-f5158065ea1e>
2008-07-23 22:06:41 +0000

    Factored out the ColorCellRenderer and ColorIcon classes that were shared by Architect and MatchMaker.

---
+++ src/ca/sqlpower/swingui/ColorIcon.java
@@ -0,0 +1,68 @@
+/*
+ * Copyright (c) 2008, SQL Power Group Inc.
+ * 
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in
+ *       the documentation and/or other materials provided with the
+ *       distribution.
+ *     * Neither the name of SQL Power Group Inc. nor the names of its
+ *       contributors may be used to endorse or promote products derived
+ *       from this software without specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package ca.sqlpower.swingui;
+
+import java.awt.Color;
+import java.awt.Component;
+import java.awt.Graphics;
+
+import javax.swing.Icon;
+
+/**
+ * This class converts a Color into an icon with the given width and height.
+ */
+public class ColorIcon implements Icon {
+    private final int width;
+    private final int height;
+
+    private Color colour;
+
+    public ColorIcon(int width, int height, Color colour) {
+        this.width = width;
+        this.height = height;
+        this.colour = colour;
+    }
+
+    public int getIconHeight() {
+        return height;
+    }
+
+    public int getIconWidth() {
+        return width;
+    }
+
+    public void paintIcon(Component c, Graphics g, int x, int y) {
+        g.setColor(colour);
+        g.fillRect(x, y, width - 1, height - 1);
+    }
+}
