e933a3f3aed32cca5e880d7600b99aa38d9e1744
dennis <Dennis@sqlpower.ca>
2008-11-06 17:47:09 +0000

    moved the CursorManager into the Library so that Wabit and Architect can both use it. This class checks the state of the mouse to see if it needs to change its image.

---
+++ src/ca/sqlpower/swingui/CursorManager.java
@@ -0,0 +1,84 @@
+/*
+ * Copyright (c) 2008, SQL Power Group Inc.
+ *
+ * This file is part of Wabit.
+ *
+ * Wabit is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Wabit is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>. 
+ */
+
+package ca.sqlpower.swingui;
+
+import java.awt.Cursor;
+
+import javax.swing.JPanel;
+
+/**
+ * A simple class that encapsulates the logic for making the cursor image
+ * look correct for the current activity.
+ */
+public class CursorManager {
+    
+    private boolean draggingTable = false;
+    private boolean dragAllModeActive = false;
+    private boolean placeModeActive = false;
+    private JPanel panel;
+    
+    public CursorManager(JPanel panel){
+    	this.panel = panel;
+    }
+    
+    public void tableDragStarted() {
+        draggingTable = true;
+        modifyCursorImage();
+    }
+    
+    public void tableDragFinished() {
+        draggingTable = false;
+        modifyCursorImage();
+    }
+    
+    public void dragAllModeStarted() {
+        dragAllModeActive = true;
+        modifyCursorImage();
+    }
+    
+    public void dragAllModeFinished() {
+        dragAllModeActive = false;
+        modifyCursorImage();
+    }
+
+    public void placeModeStarted() {
+        placeModeActive = true;
+        modifyCursorImage();
+    }
+
+    public void placeModeFinished() {
+        placeModeActive = false;
+        modifyCursorImage();
+    }
+    
+    /**
+     * Sets the appropriate cursor type based on the current
+     * state of this cursor manager.
+     */
+    private void modifyCursorImage() {
+        if (dragAllModeActive || draggingTable) {
+            panel.setCursor(Cursor.getPredefinedCursor(Cursor.MOVE_CURSOR));
+        } else if (placeModeActive) {
+            panel.setCursor(Cursor.getPredefinedCursor(Cursor.CROSSHAIR_CURSOR));
+        } else {
+            panel.setCursor(null);
+        }
+    }
+}
