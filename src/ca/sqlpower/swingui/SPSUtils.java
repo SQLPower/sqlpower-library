b7a71c31cc581aa50ebafe60eddfe3fed618a521
jeffrey <jeffrey@sqlpower.ca>
2008-12-22 22:12:37 +0000

    Added a utility method that takes a given JTree and creates a JPanel with that JTree and a SQL Power Logo at the bottom. This provides common 'branding' code for our Swing Applications.
    I also added the sqlp-72 icon into a common icons folder, and I may eventually collect all the scattered icons in all the different source packages together into this folder to keep them together.

--- src/ca/sqlpower/swingui/SPSUtils.java
+++ src/ca/sqlpower/swingui/SPSUtils.java
@@ -2,6 +2,7 @@ package ca.sqlpower.swingui;
 
 import java.awt.BasicStroke;
 import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.Component;
 import java.awt.Dialog;
 import java.awt.Dimension;
@@ -12,6 +13,8 @@ import java.awt.Rectangle;
 import java.awt.Window;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
@@ -43,7 +46,9 @@ import javax.swing.JPanel;
 import javax.swing.JPopupMenu;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
+import javax.swing.JTree;
 import javax.swing.KeyStroke;
+import javax.swing.SwingConstants;
 import javax.swing.SwingUtilities;
 import javax.swing.filechooser.FileFilter;
 import javax.swing.text.Document;
@@ -51,6 +56,9 @@ import javax.swing.text.Document;
 import org.apache.log4j.Logger;
 
 import ca.sqlpower.util.BrowserUtil;
+
+import com.jgoodies.forms.builder.DefaultFormBuilder;
+import com.jgoodies.forms.layout.FormLayout;
 
 public class SPSUtils {
 	
@@ -965,4 +973,35 @@ public class SPSUtils {
                 original.getDashPhase());
     }
 
+	/**
+	 * Returns a JPanel containing the given JTree and a JLabel positioned at
+	 * the bottom of the panel containing the SQL Power Logo that when clicked
+	 * on, will open up the SQL Power website in a web browser.
+	 * 
+	 * @param tree
+	 *            The JTree that will be added to the panel.
+	 * @return A JPanel containing the given JTree and the SQL Power Logo
+	 *         branding
+	 */
+    public static JPanel getBrandedTreePanel(JTree tree) {
+    	DefaultFormBuilder treeBuilder = new DefaultFormBuilder(new FormLayout("fill:pref:grow", "fill:pref:grow, pref"));
+    	treeBuilder.add(tree);
+    	treeBuilder.nextLine();
+    	JLabel sqlpLabel = new JLabel(new ImageIcon(SPSUtils.class.getClassLoader().getResource("icons/sqlp-72.png")));
+    	sqlpLabel.setBackground(Color.WHITE);
+    	sqlpLabel.setOpaque(true);
+    	sqlpLabel.setHorizontalAlignment(SwingConstants.LEFT);
+    	sqlpLabel.addMouseListener(new MouseAdapter() {
+    		@Override
+    		public void mouseReleased(MouseEvent e) {
+    			try {
+    				BrowserUtil.launch(SPSUtils.SQLP_URL);
+    			} catch (IOException e1) {
+    				throw new RuntimeException("Unexpected error in launch", e1); //$NON-NLS-1$
+    			}
+    		}
+		});
+		treeBuilder.add(sqlpLabel);
+		return treeBuilder.getPanel();
+    }
 }
