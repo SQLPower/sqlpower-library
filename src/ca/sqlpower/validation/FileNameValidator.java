7a6b99b61335c709710214258cd058ed530f4110
mo.jeff <mo.jeff@49f7741a-0836-0410-9614-f5158065ea1e>
2009-04-15 18:06:02 +0000

    Updated the license notices on these files to say SQL Power Library

--- src/ca/sqlpower/validation/FileNameValidator.java
+++ src/ca/sqlpower/validation/FileNameValidator.java
@@ -1,14 +1,14 @@
 /*
- * Copyright (c) 2007, SQL Power Group Inc.
+ * Copyright (c) 2008, SQL Power Group Inc.
  *
- * This file is part of Power*MatchMaker.
+ * This file is part of SQL Power Library.
  *
- * Power*MatchMaker is free software; you can redistribute it and/or modify
+ * SQL Power Library is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 3 of the License, or
  * (at your option) any later version.
  *
- * Power*MatchMaker is distributed in the hope that it will be useful,
+ * SQL Power Library is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
