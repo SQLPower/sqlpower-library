5846fca08ca7b25ff6750b57bb4a3034741ddc7d
google-groups <google-groups@sql-workbench.net>
2009-07-02 22:01:06 +0000

    First implementation of an enhanced "Generate HTML Report" dialog where the user can select a customized XSLT transformation or the built-in one

--- src/ca/sqlpower/util/XsltTransformation.java
+++ src/ca/sqlpower/util/XsltTransformation.java
@@ -16,7 +16,6 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>. 
  */
-
 package ca.sqlpower.util;
 
 import java.io.InputStream;
@@ -34,33 +33,45 @@ import javax.xml.transform.stream.StreamSource;
  * OutputStream.
  */
 public class XsltTransformation {
-    
-    private final String xsltStylesheetPath;
 
-    public XsltTransformation(String xsltStylesheetPath) {
-    	this.xsltStylesheetPath = xsltStylesheetPath;
-    }
-    
-    /**
-     * Performs an XSLT transformation, sending the results
-     * to the OutputStream result.
-     */
-    public void transform(InputStream xml, OutputStream result) throws Exception {
+	public XsltTransformation() {
+	}
 
-        InputStream xsltStylesheet = getClass().getResourceAsStream(xsltStylesheetPath);
-        Source xmlSource = new StreamSource(xml);
-        Source xsltSource = new StreamSource(xsltStylesheet);
-        
-        TransformerFactory transFact =
-                TransformerFactory.newInstance();
-        Transformer trans = transFact.newTransformer(xsltSource);
+	/**
+	 * Performs an XSLT transformation, sending the results
+	 * to the OutputStream result.
+	 *
+	 * @param builtInXsltName the name of the built-in XSLT (part of the classpath)
+	 * @param xml The XML that should be transformed
+	 * @param result the output stream where the result of the transformation should be written to
+	 */
+	public void transform(String builtInXsltName, InputStream xml, OutputStream result) throws Exception {
 
-        trans.transform(xmlSource, new StreamResult(result));
-        result.flush();
-        result.close();
-        xsltStylesheet.close();
-        xml.close();
-  
-    }
+		InputStream xsltStylesheet = getClass().getResourceAsStream(builtInXsltName);
+		transform(xsltStylesheet, xml, result);
+	}
 
+	/**
+	 * Performs an XSLT transformation, sending the results
+	 * to the OutputStream result.
+	 *
+	 * @param the XSLT that should be run
+	 * @param xml The XML that should be transformed
+	 * @param result the output stream where the result of the transformation should be written to
+	 */
+	public void transform(InputStream xsltStylesheet, InputStream xml, OutputStream result) throws Exception {
+
+		Source xmlSource = new StreamSource(xml);
+		Source xsltSource = new StreamSource(xsltStylesheet);
+
+		TransformerFactory transFact =
+				TransformerFactory.newInstance();
+		Transformer trans = transFact.newTransformer(xsltSource);
+
+		trans.transform(xmlSource, new StreamResult(result));
+		result.flush();
+		result.close();
+		xsltStylesheet.close();
+		xml.close();
+	}
 }
