a5c514c20334c24eb5e1b8f8d73333be05ce3545
terryyiu <terryyiu@49f7741a-0836-0410-9614-f5158065ea1e>
2009-11-17 18:47:39 +0000

    Refactored out the WabitJSONMessageDecoder into the library as SPJSONMessageDecoder. Also refactored out WabitJSONPersister into the library as SPJSONPersister, except the persistObject method does not throw an SPPersistenceException if parentUUID is null.

---
+++ src/ca/sqlpower/dao/json/SPJSONMessageDecoder.java
@@ -0,0 +1,199 @@
+/*
+ * Copyright (c) 2009, SQL Power Group Inc.
+ *
+ * This file is part of SQL Power Library.
+ *
+ * SQL Power Library is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * SQL Power Library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>. 
+ */
+
+package ca.sqlpower.dao.json;
+
+import java.io.ByteArrayInputStream;
+import java.io.UnsupportedEncodingException;
+
+import javax.annotation.Nonnull;
+
+import org.apache.commons.codec.binary.Base64;
+import org.apache.log4j.Logger;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import ca.sqlpower.dao.MessageDecoder;
+import ca.sqlpower.dao.SPPersistenceException;
+import ca.sqlpower.dao.SPPersister;
+import ca.sqlpower.dao.SPPersister.DataType;
+import ca.sqlpower.dao.SPPersister.SPPersistMethod;
+
+/**
+ * An implementation of {@link MessageDecoder} that takes in a String that is
+ * intended to be a JSON-formatted message, and constructs a JSONObject from it.
+ * It then expects JSONObject key values that map to {@link SPPersister}
+ * methods and their expected parameters. It then extracts this information from
+ * the JSONObject and makes the appropriate method calls to an
+ * {@link SPPersister} provided in the constructor.
+ */
+public class SPJSONMessageDecoder implements MessageDecoder<String> {
+
+	private static final Logger logger = Logger
+			.getLogger(SPJSONMessageDecoder.class);
+	
+	/**
+	 * A {@link SPPersister} that the decoder will make method calls on
+	 */
+	private SPPersister persister;
+
+	/**
+	 * Creates an SPMessageDecoder with the given {@link SPPersister}. The
+	 * messages that this class decodes will contain SPPersister method calls
+	 * with their parameters. This decoder will use the messages to make method
+	 * calls to the given SPPersister.
+	 * 
+	 * @param persister
+	 *            The {@link SPPersister} that this decoder will make method
+	 *            calls to
+	 */
+	public SPJSONMessageDecoder(@Nonnull SPPersister persister) {
+		this.persister = persister;
+	}
+
+	/**
+	 * Takes in a message in the form of String. The message is expected to be
+	 * in JSON format.
+	 * 
+	 * The JSON message is expected to be a JSONArray of JSONObjects. Each
+	 * JSONObject contains details for making a SPPersister method call.
+	 * 
+	 * It expects the following key-value pairs in each JSONObject message:
+	 * <ul>
+	 * <li>method - The String value of a {@link SPPersistMethod}. This is
+	 * used to determine which {@link SPPersister} method to call.</li>
+	 * <li>uuid - The UUID of the SPObject, if there is one, that the persist
+	 * method call will act on. If there is none, it expects
+	 * {@link JSONObject#NULL}</li>
+	 * </ul>
+	 * Other possible key-value pairs (depending on the intended method call)
+	 * include:
+	 * <ul>
+	 * <li>parentUUID</li>
+	 * <li>type</li>
+	 * <li>newValue</li>
+	 * <li>oldValue</li>
+	 * <li>propertyName</li>
+	 * </ul>
+	 * See the method documentation of {@link SPPersister} for full details
+	 * on the expected values
+	 */
+	public void decode(@Nonnull String message) throws SPPersistenceException {
+		String uuid = null;
+		JSONObject jsonObject = null;
+		try {
+			synchronized (persister) {
+				JSONArray messageArray = new JSONArray(message);
+				for (int i=0; i < messageArray.length(); i++) {
+					jsonObject = messageArray.getJSONObject(i);
+					logger.debug("Decoding Message: " + jsonObject);
+					uuid = jsonObject.getString("uuid");
+					SPPersistMethod method = SPPersistMethod.valueOf(jsonObject.getString("method"));
+					String parentUUID;
+					String propertyName;
+					DataType propertyType;
+					Object newValue;
+					switch (method) {
+					case begin:
+						persister.begin();
+						break;
+					case commit:
+						persister.commit();
+						break;
+					case persistObject:
+						parentUUID = jsonObject.getString("parentUUID");
+						String type = jsonObject.getString("type");
+						int index = jsonObject.getInt("index");
+						persister.persistObject(parentUUID, type, uuid, index);
+						break;
+					case changeProperty:
+						propertyName = jsonObject.getString("propertyName");
+						propertyType = DataType.valueOf(jsonObject.getString("type"));
+						newValue = getWithType(jsonObject, propertyType, "newValue");
+						Object oldValue = getWithType(jsonObject, propertyType, "oldValue");
+						persister.persistProperty(uuid, propertyName,
+								propertyType, oldValue, newValue);
+						break;
+					case persistProperty:
+						propertyName = jsonObject.getString("propertyName");
+						propertyType = DataType.valueOf(jsonObject.getString("type"));
+						newValue = getWithType(jsonObject, propertyType, "newValue");
+						if (newValue == null) logger.debug("newValue was null for propertyName " + propertyName);
+						persister.persistProperty(uuid, propertyName,
+								propertyType, newValue);
+						break;
+					case removeObject:
+						parentUUID = jsonObject.getString("parentUUID");
+						persister.removeObject(parentUUID, uuid);
+						break;
+					case rollback:
+						persister.rollback();
+						break;
+					default:
+						throw new SPPersistenceException(uuid,
+								"Does not support SP persistence method " + method);
+					}
+				}
+			}
+		} catch (JSONException e) {
+			if (jsonObject != null) {
+				logger.error("Error decoding JSONObject " + jsonObject);
+			}
+			throw new SPPersistenceException(uuid, e);
+		}
+	}
+
+	private static Object getNullable(@Nonnull JSONObject jo, String propName) throws JSONException {
+		final Object value = jo.get(propName);
+		if (value == JSONObject.NULL) {
+			return null;
+		} else {
+			return value;
+		}
+	}
+	
+	private static Object getWithType(@Nonnull JSONObject jo, DataType type, String propName) throws JSONException {
+		if (getNullable(jo, propName) == null) return null;
+		
+		switch (type) {
+		case BOOLEAN:
+			return Boolean.valueOf(jo.getBoolean(propName));
+		case DOUBLE:
+			return Double.valueOf(jo.getDouble(propName));
+		case INTEGER:	
+			return Integer.valueOf(jo.getInt(propName));
+		case PNG_IMG:
+			getNullable(jo, propName);
+			String base64Data = jo.getString(propName);
+			byte[] decodedBytes;
+			try {
+				decodedBytes = Base64.decodeBase64(base64Data.getBytes("ascii"));
+			} catch (UnsupportedEncodingException e) {
+				throw new RuntimeException("ASCII should always be supported!", e);
+			}
+			return new ByteArrayInputStream(decodedBytes);
+		case NULL:
+		case STRING:
+		case REFERENCE:
+		default:
+			return getNullable(jo, propName);
+		}
+	}
+}
---
+++ src/ca/sqlpower/dao/json/SPJSONPersister.java
@@ -0,0 +1,295 @@
+/*
+ * Copyright (c) 2009, SQL Power Group Inc.
+ *
+ * This file is part of SQL Power Library.
+ *
+ * SQL Power Library is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * SQL Power Library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>. 
+ */
+
+package ca.sqlpower.dao.json;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.commons.codec.binary.Base64;
+import org.apache.log4j.Logger;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import ca.sqlpower.dao.MessageSender;
+import ca.sqlpower.dao.SPPersistenceException;
+import ca.sqlpower.dao.SPPersister;
+import ca.sqlpower.util.SQLPowerUtils;
+
+/**
+ * A {@link SPPersister} implementation that serializes
+ * {@link SPPersister} method calls as {@link JSONObject}s and transmits them
+ * to a destination using a {@link MessageSender}. This allows these method
+ * calls to be transmitted to other systems, typically (but not necessarily)
+ * over a network connection.
+ */
+public class SPJSONPersister implements SPPersister {
+
+	private static final Logger logger = Logger
+			.getLogger(SPJSONPersister.class);
+	
+	/**
+	 * A count of transactions, mainly to keep track of nested transactions.
+	 */
+	private int transactionCount = 0;
+	
+	/**
+	 * A MessagePasser object that is responsible for transmitting the
+	 * JSONObject contents.
+	 */
+	private final MessageSender<JSONObject> messageSender;
+
+	private final List<JSONObject> messageBuffer;
+	
+	/**
+	 * Create a {@link SPJSONPersister} that uses the given
+	 * {@link MessageSender} to transmit the JSON content
+	 */
+	public SPJSONPersister(MessageSender<JSONObject> messageSender) {
+		this.messageSender = messageSender;
+		this.messageBuffer = new ArrayList<JSONObject>();
+	}
+	
+	public void begin() throws SPPersistenceException{
+		JSONObject jsonObject = new JSONObject();
+		try {
+			jsonObject.put("method", SPPersistMethod.begin);
+			// Need to put this in or anything calling get on the key "uuid"
+			// will throw a JSONException
+			jsonObject.put("uuid", JSONObject.NULL);
+		} catch (JSONException e) {
+			logger.error("Exception encountered while building JSON message. Rollback initiated.",e);
+			rollback();
+			throw new SPPersistenceException(null, e);
+		}
+		logger.debug(jsonObject);
+		messageBuffer.add(jsonObject);
+		transactionCount++;
+	}
+
+	public void commit() throws SPPersistenceException {
+		if (transactionCount == 0) {
+			throw new SPPersistenceException(null, "Commit attempted while not in a transaction");
+		}
+		JSONObject jsonObject = new JSONObject();
+		try {
+			jsonObject.put("method", SPPersistMethod.commit);
+			// Need to put this in or anything calling get on the key "uuid"
+			// will throw a JSONException
+			jsonObject.put("uuid", JSONObject.NULL);
+		} catch (JSONException e) {
+			logger.error("Exception encountered while building JSON message. Rollback initiated.",e);
+			rollback();
+			throw new SPPersistenceException(null, e);
+		}
+		try {
+			logger.debug(jsonObject);
+			messageBuffer.add(jsonObject);
+			if (transactionCount == 1) {
+				for (JSONObject obj: messageBuffer) {
+					messageSender.send(obj);
+				}
+				messageSender.flush();
+				messageBuffer.clear();
+				transactionCount = 0;
+			} else {
+				transactionCount--;
+			}
+		} catch (Throwable t) {
+			logger.error("Exception encountered while building JSON message. Rollback initiated.",t);
+			messageBuffer.clear();
+			messageSender.clear();
+			transactionCount = 0;
+			rollback();
+			if (t instanceof SPPersistenceException) {
+				throw (SPPersistenceException) t;
+			} else if (t instanceof RuntimeException) {
+				throw (RuntimeException) t;
+			} else {
+				throw new RuntimeException(t);
+			}
+		}
+	}
+
+	/**
+	 * XXX Any JSON persisters that extend this class must override this method
+	 * before calling super.{@link #persistObject(String, String, String, int)}
+	 * if null parentUUIDs need to be handled correctly.
+	 */
+	public void persistObject(String parentUUID, String type, String uuid, int index)
+			throws SPPersistenceException {
+		
+		// XXX Not handling null parentUUIDs here. Extending classes should handle them before calling super.persistObject 
+		
+		if (transactionCount == 0) {
+			throw new SPPersistenceException("Operation attempted while not in a transaction.");
+		}
+		JSONObject jsonObject = new JSONObject();
+		try {
+			jsonObject.put("method", SPPersistMethod.persistObject.toString());
+			if (parentUUID == null) {
+				jsonObject.put("parentUUID", JSONObject.NULL);
+			} else {
+				jsonObject.put("parentUUID", parentUUID);
+			}
+			jsonObject.put("type", type);
+			jsonObject.put("uuid", uuid);
+			jsonObject.put("index", index);
+		} catch (JSONException e) {
+			logger.error(e);
+			rollback();
+			throw new SPPersistenceException(uuid, e);
+		}
+		logger.debug(jsonObject);
+		messageBuffer.add(jsonObject);
+	}
+
+	public void persistProperty(String uuid, String propertyName, DataType type,
+			Object oldValue, Object newValue) throws SPPersistenceException {
+		if (transactionCount == 0) {
+			throw new SPPersistenceException("Operation attempted while not in a transaction.");
+		}
+		JSONObject jsonObject = new JSONObject();
+		try {
+			jsonObject.put("method", SPPersistMethod.changeProperty);
+			jsonObject.put("uuid", uuid);
+			jsonObject.put("propertyName", propertyName);
+			jsonObject.put("type", type.toString());
+			setValueInJSONObject(jsonObject, "oldValue", type, oldValue);
+			setValueInJSONObject(jsonObject, "newValue", type, newValue);
+		} catch (JSONException e) {
+			logger.error(e);
+			rollback();
+			throw new SPPersistenceException(uuid, e);
+		} catch (IOException e) {
+			logger.error(e);
+			rollback();
+			throw new SPPersistenceException(uuid, e);
+		}
+		logger.debug(jsonObject);
+		messageBuffer.add(jsonObject);
+	}
+	
+	public void persistProperty(String uuid, String propertyName, DataType type, Object newValue) throws SPPersistenceException {
+		if (transactionCount == 0) {
+			throw new SPPersistenceException("Operation attempted while not in a transaction.");
+		}
+		JSONObject jsonObject = new JSONObject();
+		try {
+			jsonObject.put("method", SPPersistMethod.persistProperty);
+			jsonObject.put("uuid", uuid);
+			jsonObject.put("propertyName", propertyName);
+			jsonObject.put("type", type.toString());
+			setValueInJSONObject(jsonObject, "newValue", type, newValue);
+		} catch (JSONException e) {
+			logger.error(e);
+			rollback();
+			throw new SPPersistenceException(uuid, e);
+		} catch (IOException e) {
+			logger.error(e);
+			rollback();
+			throw new SPPersistenceException(uuid, e);
+		}
+		logger.debug(jsonObject);
+		messageBuffer.add(jsonObject);
+	}
+
+	/**
+	 * Sets the named property of the given JSON object to the given value. This
+	 * is a nontrivial operation because JSON nulls are special, and so are
+	 * values of type PNG_IMAGE.
+	 * 
+	 * @param jsonObject
+	 *            The object whose property to set.
+	 * @param jsonPropName
+	 *            The property name to set on jsonObject.
+	 * @param type
+	 *            the SPPersister framework datatype for value.
+	 * @param value
+	 *            The actual value to set. Values for all possible DataTypes are
+	 *            properly converted to a JavaScript data type before being set.
+	 */
+	private void setValueInJSONObject(JSONObject jsonObject, String jsonPropName, DataType type, Object value)
+	throws IOException, JSONException, UnsupportedEncodingException {
+		if (type == DataType.PNG_IMG && value != null) {
+			ByteArrayInputStream in = (ByteArrayInputStream) value;
+			ByteArrayOutputStream out = new ByteArrayOutputStream();
+			SQLPowerUtils.copyStream(in, out);
+			byte[] bytes = out.toByteArray();
+			byte[] base64Bytes = Base64.encodeBase64(bytes);
+			jsonObject.put(jsonPropName, new String(base64Bytes, "ascii"));
+		} else {
+			jsonObject.put(jsonPropName, value == null ? JSONObject.NULL : value);
+		}
+	};
+	
+	public void removeObject(String parentUUID, String uuid)
+			throws SPPersistenceException {
+		if (transactionCount == 0) {
+			throw new SPPersistenceException("Operation attempted while not in a transaction.");
+		}
+		JSONObject jsonObject = new JSONObject();
+		try {
+			jsonObject.put("method", SPPersistMethod.removeObject);
+			jsonObject.put("parentUUID", parentUUID);
+			jsonObject.put("uuid", uuid);
+		} catch (JSONException e) {
+			logger.error(e);
+			rollback();
+			throw new SPPersistenceException(uuid, e);
+		}
+		logger.debug(jsonObject);
+		messageBuffer.add(jsonObject);
+	}
+	
+	public void rollback() {
+		JSONObject jsonObject = new JSONObject();
+		try {
+			// First we empty messages cues so we only send a rollback message
+			messageBuffer.clear();
+			messageSender.clear();
+			// Need to put this in or anything calling get on the key "uuid"
+			// will throw a JSONException
+			jsonObject.put("method", SPPersistMethod.rollback);
+			jsonObject.put("uuid", JSONObject.NULL);
+			logger.debug(jsonObject);
+			messageBuffer.add(jsonObject);
+			for (JSONObject obj: messageBuffer) {
+				messageSender.send(obj);
+			}
+			messageSender.flush();
+		} catch (JSONException e) {
+			throw new RuntimeException("Could not create rollback message to send. Bad bad bad.", e);
+		} catch (SPPersistenceException e) {
+			throw new RuntimeException("Could not create rollback message to send. Bad bad bad.", e);
+		} finally {
+			messageBuffer.clear();
+			messageSender.clear();
+			transactionCount = 0;
+		}
+	}
+	
+	public MessageSender<JSONObject> getMessageSender() {
+		return messageSender;
+	}
+}
