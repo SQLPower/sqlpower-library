9d6805276cfe5f635e9920f73aaee7b6736d085f
lucboudreau <lucboudreau@49f7741a-0836-0410-9614-f5158065ea1e>
2009-12-16 17:21:29 +0000

    First draft of the SPVariableResolver interface. Pending review.

---
+++ src/ca/sqlpower/object/SPVariableResolver.java
@@ -0,0 +1,179 @@
+/*
+ * Copyright (c) 2009, SQL Power Group Inc.
+ *
+ * This file is part of SQL Power Library.
+ *
+ * SQL Power Library is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * SQL Power Library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>. 
+ */
+
+package ca.sqlpower.object;
+
+import java.util.Collection;
+
+/**
+ * Defines all methods to implement to make an object capable of resolving
+ * variables into actual values.
+ * 
+ * <p>Variables keys are {@link String} values and might be prefixed by a 
+ * namespace value. Typically, the namespace will be a specific object UUID.
+ * You can ask the resolver directly if he is capable of resolving variables
+ * in a given namespace by calling {@link SPVariableResolver#resolvesNamespace(String)}.
+ * 
+ * <p>For example, one might expect a lookup key to be something like:
+ * 
+ * <blockquote><code>w1234-1234-1234-1234:myVariableKey</code></blockquote>
+ * 
+ * <p>For performance reasons, it is strongly suggested of using those namespaces
+ * and cache a map of namespaces<->resolvers. Large object models will benefit greatly
+ * of this.
+ * 
+ * @author Luc Boudreau
+ */
+public interface SPVariableResolver {
+
+	/**
+	 * Resolves a variable to it's value by a String key.
+	 * Returns null if the variable cannot be resolved.
+	 * 
+	 * <p>If more than one value is matched by the key, the actual returned
+	 * value is not guaranteed to be neither the first match or anything
+	 * alike. You should use {@link SPVariableResolver#resolveCollection(String)}
+	 * if you expect there might be more than one possible returned values.
+	 * 
+	 * @param key The variable key to resolve
+	 * @return The variable object, or null if it cannot be resolved.
+	 */
+	public Object resolve(String key);
+	
+	/**
+	 * Resolves a variable to it's value by a String key.
+	 * Returns the default supplied value if the variable cannot be resolved.
+	 * 
+	 * <p>If more than one value is matched by the key, the actual returned
+	 * value is not guaranteed to be neither the first match or anything
+	 * alike. You should use {@link SPVariableResolver#resolveCollection(String, Object)}
+	 * if you expect there might be more than one possible returned values.
+	 * 
+	 * <p>A common mistake done by using this call is to receive the default
+	 * value without verifying prior to the call if the implementing resolver
+	 * can actually resolve it via the {@link SPVariableResolver#resolves(String)} 
+	 * function.
+	 * 
+	 * @param key The variable key to resolve
+	 * @param defaultValue The default value to return if the variable 
+	 * cannot be resolved
+	 * @return The variable object, or the default value passed as
+	 * a parameter if it cannot be resolved.
+	 */
+	public Object resolve(String key, Object defaultValue);
+	
+	/**
+	 * Resolves a variable to it's value by a String key.
+	 * Returns an empty {@link Collection} if the variable cannot be resolved.
+	 * 
+	 * @param key The variable key to resolve
+	 * @return The variable object, or an empty {@link Collection}
+	 * if it cannot be resolved.
+	 */
+	public Collection<Object> resolveCollection(String key);
+	
+	/**
+	 * Resolves a variable to it's value by a String key.
+	 * Returns a {@link Collection} containing only the supplied default value
+	 * if the variable cannot be resolved.
+	 * 
+	 * <p>A common mistake done by using this call is to receive the default
+	 * value without verifying prior to the call if the implementing resolver
+	 * can actually resolve it via the {@link SPVariableResolver#resolves(String)} 
+	 * function.
+	 * 
+	 * @param key The variable key to resolve
+	 * @return The variable object, or an empty {@link Collection}
+	 * if it cannot be resolved.
+	 */
+	public Collection<Object> resolveCollection(String key, Object defaultValue);
+	
+	/**
+	 * Verifies if this variable resolver can effectively resolve the
+	 * supplied variable.
+	 * @param key The key of the variable we would like to know if it 
+	 * can be resolved.
+	 * @return True or false.
+	 */
+	public boolean resolves(String key);
+	
+	/**
+	 * Verifies if this variable resolver can effectively resolve 
+	 * variables in the provided namespace value.
+	 * @param namespace The namespace for which we want to know if this resolver
+	 * is capable of resolving variables.
+	 * @return True or false.
+	 */
+	public boolean resolvesNamespace(String namespace);
+	
+	/**
+	 * Tries to resolve a variable and returns a {@link Collection} of partial
+	 * matches which keys first characters match the supplied key.
+	 * If no matches are found, an empty {@link Collection} is returned.
+	 * This is typically used for content a assist feature.
+	 * 
+	 * <p> For example, if we supply "foo" as a key parameter,
+	 * both "foo" and "foobar" would be matched and returned in the
+	 * matches collection.
+	 *  
+	 * @param key The partial key value for which we want all matching
+	 * variables values.
+	 * @return A collection of matching variables.
+	 */
+	public Collection<Object> matches(String key);
+	
+	/**
+	 * Tries to resolve a variable from a regular expression expressed
+	 * as a String object and returns a collection of partial matches. If no matches
+	 * are found, an empty {@link Collection} is returned.
+	 * This is typically used for content a assist feature.
+	 * 
+	 * <p> For example, if we supply "foo" as a key parameter,
+	 * both "foo" and "foobar" would be matched and returned in the
+	 * matches collection.
+	 *  
+	 * @param key The partial key value for which we want all matching
+	 * variables values.
+	 * @return A collection of matching variables.
+	 */
+	public Collection<Object> matches(String key, boolean isRegexp);
+	
+	/**
+	 * Because variable resolvers might be chained, this method should
+	 * return the proper variable resolver who is capable of effectively
+	 * resolving variables in the provided namespace. Most variable 
+	 * resolvers will simply return themselves.
+	 * 
+	 * <p>This is typical of large object models where you would call the
+	 * root of the model to resolve variables. For performance reasons, one might
+	 * prefer to ask directly the last delegate in the chain instead of trigerring
+	 * an object lookup each time for each delegate in the chain every time a
+	 * variable needs to be resolved. This way, you would ask the root node for the
+	 * delegate and then ask this delegate to resolve the variable.
+	 * 
+	 * <blockquote><code>root.getResolverDelegate("namespaceId").resolve("key");</code></blockquote>
+	 * 
+	 * <p>If more than one delegate can resolve the supplied namespace,
+	 * there is no guarantee whatsoever as to which will be used.
+	 * 
+	 * @param namespace The namespace for which we want the resolver delegate.
+	 * @return The resolver delegate.
+	 */
+	public SPVariableResolver getResolverDelegate(String namespace);
+}
