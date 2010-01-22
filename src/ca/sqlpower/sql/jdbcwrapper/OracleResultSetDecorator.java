aa8f047c875836ea026a2139d7232a60e148b461
lucboudreau <lucboudreau@49f7741a-0836-0410-9614-f5158065ea1e>
2010-01-22 19:37:11 +0000

    Fixes bug 2368 - Oracle Timestamps not shown properly
    Adds conversion code to the wrapper.

--- src/ca/sqlpower/sql/jdbcwrapper/OracleResultSetDecorator.java
+++ src/ca/sqlpower/sql/jdbcwrapper/OracleResultSetDecorator.java
@@ -1,8 +1,13 @@
 package ca.sqlpower.sql.jdbcwrapper;
 
+import java.lang.reflect.Method;
 import java.sql.ResultSet;
 import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
 import java.sql.Statement;
+import java.sql.Timestamp;
+import java.util.Calendar;
+import java.util.Map;
 
 public class OracleResultSetDecorator extends ResultSetDecorator {
 
@@ -17,5 +22,102 @@ public class OracleResultSetDecorator extends ResultSetDecorator {
 			ResultSetMetaData rsmd) {
 		return new OracleResultSetMetaDataDecorator(rsmd);
 	}
+	
+	@Override
+	public Object getObject(int columnIndex) throws SQLException {
+		Object returnValue = super.getObject(columnIndex);
+		if (returnValue != null &&
+				returnValue.getClass().getCanonicalName().equals("oracle.sql.TIMESTAMP")) {
+			return this.getTimestamp(columnIndex);
+		} else {
+			return returnValue;
+		}
+	}
+	
+	@Override
+	public Object getObject(int i, Map<String, Class<?>> map)
+			throws SQLException {
+		Object returnValue = super.getObject(i, map);
+		if (returnValue != null &&
+				returnValue.getClass().getCanonicalName().equals("oracle.sql.TIMESTAMP")) {
+			return this.getTimestamp(i);
+		} else {
+			return returnValue;
+		}
+	}
+	
+	@Override
+	public Object getObject(String colName, Map<String, Class<?>> map)
+			throws SQLException {
+		Object returnValue = super.getObject(colName, map);
+		if (returnValue != null &&
+				returnValue.getClass().getCanonicalName().equals("oracle.sql.TIMESTAMP")) {
+			return this.getTimestamp(colName);
+		} else {
+			return returnValue;
+		}
+	}
+	
+	@Override
+	public Object getObject(String columnName) throws SQLException {
+		Object returnValue = super.getObject(columnName);
+		if (returnValue != null &&
+				returnValue.getClass().getCanonicalName().equals("oracle.sql.TIMESTAMP")) {
+			return this.getTimestamp(columnName);
+		} else {
+			return returnValue;
+		}
+	}
 
+	@Override
+	public Timestamp getTimestamp(int columnIndex) throws SQLException {
+		return convertTimestamp(super.getTimestamp(columnIndex), null);
+	}
+	
+	@Override
+	public Timestamp getTimestamp(int columnIndex, Calendar cal)
+			throws SQLException {
+		return convertTimestamp(super.getTimestamp(columnIndex, cal), null);
+	}
+	
+	@Override
+	public Timestamp getTimestamp(String columnName) throws SQLException {
+		return convertTimestamp(super.getTimestamp(columnName), null);
+	}
+	
+	@Override
+	public Timestamp getTimestamp(String columnName, Calendar cal)
+			throws SQLException {
+		return convertTimestamp(super.getTimestamp(columnName, cal), cal);
+	}
+	
+	private Timestamp convertTimestamp(Timestamp oracleTimestamp, Calendar cal)
+			throws SQLException 
+	{
+		try {
+			if (oracleTimestamp.getClass().getCanonicalName().equals("oracle.sql.TIMESTAMP")) {
+				if (cal == null) {
+					Method getter = oracleTimestamp.getClass().getDeclaredMethod("timestampValue");
+					return (Timestamp)getter.invoke(oracleTimestamp);
+				} else {
+					Method getter = oracleTimestamp.getClass().getDeclaredMethod("timestampValue", cal.getClass());
+					return (Timestamp)getter.invoke(oracleTimestamp, cal);
+				}
+			} else {
+				return oracleTimestamp;
+			}
+		} catch (SecurityException e) {
+			SQLException ex = new SQLException("Cannot convert Oracle Timestamp class because of a security exception: " + e.getMessage());
+			ex.setStackTrace(e.getStackTrace());
+			throw ex;
+		} catch (NoSuchMethodException e) {
+			SQLException ex = new SQLException("Cannot convert Oracle Timestamp class because the getTimestamp method could not be found: " + e.getMessage());
+			ex.setStackTrace(e.getStackTrace());
+			throw ex;
+		} catch (Exception e) {
+			SQLException ex = new SQLException("Cannot convert Oracle Timestamp class because the an exception was encountered: " + e.getMessage());
+			ex.setStackTrace(e.getStackTrace());
+			throw ex;
+		}
+	}
 }
