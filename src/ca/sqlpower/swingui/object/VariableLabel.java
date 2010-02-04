df2f41232993fb8b02b4bc60a3977f714bd84b2a
lucboudreau <lucboudreau@49f7741a-0836-0410-9614-f5158065ea1e>
2010-02-04 17:34:33 +0000

    Fixes and safety improvements to the var labels.

--- src/ca/sqlpower/swingui/object/VariableLabel.java
+++ src/ca/sqlpower/swingui/object/VariableLabel.java
@@ -21,6 +21,7 @@
 package ca.sqlpower.swingui.object;
 
 import java.awt.Color;
+import java.awt.Component;
 import java.awt.Graphics;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
@@ -31,7 +32,6 @@ import javax.swing.text.BadLocationException;
 import javax.swing.text.Document;
 import javax.swing.text.Style;
 import javax.swing.text.StyleConstants;
-import javax.swing.text.StyleContext;
 import javax.swing.text.StyledDocument;
 
 import ca.sqlpower.object.SPVariableHelper;
@@ -41,7 +41,11 @@ import ca.sqlpower.swingui.DataEntryPanelBuilder;
 
 public class VariableLabel extends JLabel {
 	
-	public static String STYLE_NAME = "sqlp-variable-style";
+	public final static String STYLE_NAME = "sqlp-variable-style";
+	public final static String VAR_VALUE = "sqlp-variable-value";
+	public final static String UUID = "sqlp-variable-uuid";
+	public final static String VARIABLE_CHAR = "\u0001";
+	
 	
 	private final static int MARGIN = 10;
 
@@ -62,40 +66,6 @@ public class VariableLabel extends JLabel {
 		this.variableDef = variableDef;
 		setAlignmentY(BOTTOM);
 		setHorizontalAlignment(JLabel.CENTER);
-		
-		// Open the editor if the user clicks on the label
-		super.addMouseListener(new MouseListener() {
-			public void mouseReleased(MouseEvent e) {
-			}
-			public void mousePressed(MouseEvent e) {
-			}
-			public void mouseExited(MouseEvent e) {
-			}
-			public void mouseEntered(MouseEvent e) {
-			}
-			public void mouseClicked(MouseEvent e) 
-			{
-				VariablesPanel vp = 
-					new VariablesPanel(
-							helper,
-							new VariableInserter() {
-								public void insert(String variable) {
-									VariableLabel.this.variableDef = variable;
-									VariableLabel.this.setText(variable);
-									VariableLabel.this.getParent().repaint();
-								}
-							});
-				
-				JDialog dialog = 
-					DataEntryPanelBuilder.createDataEntryPanelDialog(
-						vp,
-				        VariableLabel.this.getParent(), 
-				        "Edit variable", 
-				        "Update");
-				
-				dialog.setVisible(true);
-			}
-		});
 	}
 	
 	@Override
@@ -113,6 +83,11 @@ public class VariableLabel extends JLabel {
 		return "${"+this.variableDef+"}";
 	}
 	
+	public void setVariableDef(String variableDef) {
+		this.variableDef = variableDef;
+		this.setText("  " + SPVariableHelper.getKey(VariableLabel.this.variableDef) + "  > ");
+	}
+	
 	/**
 	 * Helper method to insert and register a styled label in
 	 * a document
@@ -125,14 +100,124 @@ public class VariableLabel extends JLabel {
 	 * @throws BadLocationException If the position supplied is not a valid
 	 * position within the target document.
 	 */
-	public static void insertVariable(
-			String variableDef, 
+	public static void insertLabel(
+			final String variableDef, 
+			final SPVariableHelper helper,
+			final StyledDocument target,
+			final int insertPosition,
+			final Component dialogOwner) throws BadLocationException 
+	{
+		final String uuid = java.util.UUID.randomUUID().toString();
+		final Style varStyle = target.addStyle(STYLE_NAME, null);
+		
+		varStyle.addAttribute(VAR_VALUE, variableDef);
+		varStyle.addAttribute(UUID, uuid);
+		final VariableLabel labelToInsert = new VariableLabel(variableDef, helper);
+		StyleConstants.setComponent(varStyle, labelToInsert);
+		target.insertString(insertPosition, VARIABLE_CHAR, varStyle);
+		
+		// Open the editor if the user clicks on the label
+		labelToInsert.addMouseListener(new MouseListener() {
+			public void mouseReleased(MouseEvent e) {
+			}
+			public void mousePressed(MouseEvent e) {
+			}
+			public void mouseExited(MouseEvent e) {
+			}
+			public void mouseEntered(MouseEvent e) {
+			}
+			public void mouseClicked(MouseEvent e) 
+			{
+				VariablesPanel vp = 
+					new VariablesPanel(
+							helper,
+							new VariableInserter() {
+								public void insert(String variable) {
+									// We don't know where the var is now, but we can search it
+									// by UUID attribute.
+									for (int i = 0; i < target.getLength(); i++) {
+										try {
+											if (target.getCharacterElement(i).getAttributes().isDefined(UUID)
+													&& target.getCharacterElement(i).getAttributes().getAttribute(UUID).equals(uuid)
+													&& target.getText(i, 1).equals(VARIABLE_CHAR)) {
+												// Gotcha!
+												target.remove(i, 1);
+												String newVariableDef = variable.replaceFirst("\\$", "").replaceFirst("\\{", "").replaceFirst("\\}", "");
+												insertLabel(newVariableDef, helper, target, i, dialogOwner);
+											}
+										} catch (BadLocationException e) {
+											throw new AssertionError(e);
+										}
+									}
+								}
+							},
+							variableDef);
+				
+				JDialog dialog = 
+					DataEntryPanelBuilder.createDataEntryPanelDialog(
+						vp,
+				        dialogOwner, 
+				        "Edit variable", 
+				        "Update");
+				
+				dialog.setVisible(true);
+			}
+		});
+	}
+	
+	/**
+	 * Takes a {@link StyledDocument} object and switches all the
+	 * variables labels for textual representations of the variables.
+	 * @param target
+	 */
+	public static void removeLabels(StyledDocument target) {
+		try {
+			for (int i = 0; i < target.getLength(); i++) {
+				if (target.getCharacterElement(i).getAttributes().isDefined(VAR_VALUE)
+						&& target.getText(i, 1).equals(VARIABLE_CHAR)) {
+					String varValue = (String)target.getCharacterElement(i).getAttributes().getAttribute(VAR_VALUE);
+					target.remove(i, 1);
+					target.insertString(
+							i, 
+							"${"+varValue+"}", 
+							null);
+				}
+			}
+		
+		} catch (BadLocationException e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	
+	
+	public static void insertLabels(
 			SPVariableHelper helper,
 			StyledDocument target,
-			int insertPosition) throws BadLocationException 
+			Component dialogOwner)
 	{
-		Style varStyle = target.addStyle(STYLE_NAME, target.getStyle(StyleContext.DEFAULT_STYLE));
-		StyleConstants.setComponent(varStyle, new VariableLabel(variableDef, helper));
-		target.insertString(insertPosition, "e", varStyle);
+		try {
+			
+			while (true) 
+			{
+				String text = target.getText(0, target.getLength());
+				int indexStart = text.indexOf("${");
+				int indexEnd = text.indexOf("}");
+				
+				if (indexStart == -1) {
+					break;
+				}
+				
+				String var = text.substring(indexStart, indexEnd + 1);
+				var = var.replaceFirst("\\$", "").replaceFirst("\\{", "").replaceFirst("\\}", "");
+				
+				target.remove(indexStart, indexEnd - indexStart + 1);
+				
+				insertLabel(var, helper, target, indexStart, dialogOwner);
+			}
+
+		} catch (BadLocationException e) {
+			throw new AssertionError(e);
+		}
 	}
 }
