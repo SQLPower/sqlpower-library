c07e05b8586c43bec4e00b30833c7c185c47a5ce
silva.josemanuel1 <silva.josemanuel1@gmail.com>
2010-02-11 21:40:20 +0000

    Moved TransactionInformation to sqlpower library

---
+++ src/ca/sqlpower/enterprise/TransactionInformation.java
@@ -0,0 +1,46 @@
+package ca.sqlpower.enterprise;
+
+import java.util.Date;
+
+/**
+ * Simple container class for information regarding a particular revision.
+ */
+public class TransactionInformation {
+
+    private final long versionNumber;
+    private final Date timeCreated;
+    private final String versionAuthor;
+    private final String versionDescription;
+    
+    public TransactionInformation(long versionNumber, Date timeCreated, String versionAuthor, String versionDescription) {
+        this.versionNumber = versionNumber;
+        this.timeCreated = timeCreated;
+        this.versionAuthor = versionAuthor;
+        this.versionDescription = versionDescription;
+    }
+    
+    /**
+     * Returns a formatted list of strings describing this transaction.
+     */
+    public String toString() {
+        return "v" + versionNumber + " (" + timeCreated.toString() + ")" +
+                ", " + versionAuthor + ":" + versionDescription;
+    }
+
+    public long getVersionNumber() {
+        return versionNumber;
+    }
+
+    public Date getTimeCreated() {
+        return timeCreated;
+    }
+
+    public String getVersionAuthor() {
+        return versionAuthor;
+    }
+
+    public String getVersionDescription() {
+        return versionDescription;
+    }
+    
+}
