452aa7e4ba5e6bbc71d0094195149f35d0c9bb2e
silva.josemanuel1 <silva.josemanuel1@gmail.com>
2010-03-18 18:38:43 +0000

    Change license comment to say SQL Power Library instead of Wabit.

--- src/ca/sqlpower/dao/session/Point2DConverter.java
+++ src/ca/sqlpower/dao/session/Point2DConverter.java
@@ -1,14 +1,14 @@
 /*
  * Copyright (c) 2009, SQL Power Group Inc.
  *
- * This file is part of Wabit.
+ * This file is part of SQL Power Library.
  *
- * Wabit is free software; you can redistribute it and/or modify
+ * SQL Power Library is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 3 of the License, or
  * (at your option) any later version.
  *
- * Wabit is distributed in the hope that it will be useful,
+ * SQL Power Library is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
