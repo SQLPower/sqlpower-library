64e2370b77c7d22bf661285f9c088294f70d574e
terryyiu <terryyiu@gmail.com>
2010-08-23 15:26:44 +0000

    Changed BrowserUtil to delegate to Desktop's browser(URI) method now that we've switched over to Java 6.

--- src/main/java/ca/sqlpower/util/BrowserUtil.java
+++ src/main/java/ca/sqlpower/util/BrowserUtil.java
@@ -1,37 +1,20 @@
 package ca.sqlpower.util;
 
+import java.awt.Desktop;
 import java.io.IOException;
+import java.net.URI;
 
 /**
  * Utility class for dealing with Web Browsers.
- * For now uses operating system browser and knows about os.name;
- * when Java 6 becomes common, should delegate to java.awt.Desktop.
  */
 public class BrowserUtil {
-
-    final static String OS_NAME = System.getProperty("os.name");
-    final static String OS_VER = System.getProperty("os.version");
 
     /** Launches the default browser to display a URL.
      * @throws IOException
      */
     public static void launch(String uri) throws IOException {
-        Runtime runtime = Runtime.getRuntime();
-        
-        if (OS_NAME.contains("Windows")) {
-            runtime.exec("cmd /C \"start " + uri + "\"");
-        } else if (OS_NAME.startsWith("Mac OS")) {
-            runtime.exec("open " + uri);
-        } else {
-        	String[] browsers = {"xdg-open ", "epiphany", "firefox", "mozilla", "konqueror",
-			"netscape","opera"};
-
-	    	// Build a command string which looks like "browser1 "url" || browser2 "url" ||..."
-	    	StringBuffer cmd = new StringBuffer();
-	    	for (int i=0; i<browsers.length; i++)
-	    		cmd.append( (i==0  ? "" : " || " ) + browsers[i] +" \"" + uri + "\" ");
-	
-	    	runtime.exec(new String[] { "sh", "-c", cmd.toString() });
-        }
+    	if (Desktop.isDesktopSupported()) {
+    		Desktop.getDesktop().browse(URI.create(uri));
+    	}
     }
 }
